3dcd7b156f94c0138164ae40fc8eb68d1895bd53
Malcolm Schonfield <malcolm_schonfield@yahoo.fr>
2025-10-02 15:56:51 +0200

    add John Doherty’s Mazurka

--- src/tunes.json.js
+++ src/tunes.json.js
@@ -592,5 +592,49 @@ M: 3/4
 |:FA dc d2|FA dA (3Bcd|FA dc ~d2|BG EF GE|
 FA dc d2|FA dA (3Bcd|ec dA FA|1 BG EF GE:|2 [M: 1/2]BG E!D.C.!G ||
 `, references:[{notes:`1. A crooked tune. 2. See [more comments & links I put on the session](https://thesession.org/tunes/8479#comment970217)`}]}
+, {abc:`
+X: 1
+T: John Doherty’s Mazurka
+S: John Doherty
+D: Bundle and Go
+F: https://open.spotify.com/track/14llNbrqpfrhKkBB2Q94qx
+N: * This is a “crooked” tune. It’s mostly in three, but there are two bars 
+N: in two beats, in the middle.
+N: * This is just a rough approximation of Doherty’s playing. No attempt is made
+N: to get his decorations, embellishments, subtleties of phrasing etc which make up such a 
+N: vital part of the music.
+N: * Swing: sometimes two quavers are swung a bit here; and sometimes not!
+N: * [A transcription](https://thesession.org/tunes/2497#setting15797) by ceolachan 
+N: on thesession.org was used as a starting point.
+N: * The track notes for the album are shared here: https://thesession.org/recordings/344#comment756354.
+N: They have this info about this mazurka:
+N: “Learned from the playing of a travelling Antrim fiddler, Francie Welsch or Walsh, 
+N: this evocative tune is unique in the Irish tradition for its transition 
+N: from ¾ mazurka signature to slip jig tempo.”
+N: Perhaps the “slip jig tempo” parts mentioned are the C and D sections. I would perhaps prefer
+N: to use the term “hop jig tempo”.
+Z: abc-transcription Malcolm Schonfield % 2024-05-29; 2025-03-26
+Z: abc-copyright CC BY-NC 2.0 (https://creativecommons.org/licenses/by-nc/2.0/fr/deed.en)
+I:abc-charset utf-8
+M: 3/4
+L: 1/8
+Q: 1/4=180
+R: mazurka / hop jig
+K: A
+%swing 0.25 0
+P:A
+|: E AB | !segno! cd cE Ac | ea ec df | B2 Ba gf | fe e2 EA |
+cd cE Ac | ea ec df | Bc BA GB | A2 A :| E Ac | 
+P:B
+e2 ec (3efg | a2 ab (3agf | e2 ea (3gfe | d2 dg (3fed | c2 cB Ac |
+e2 ec (3efg | a2 ab (3agf | e2 ea (3gfe | d2 cB cd |[M:2/4] e2 B2 || 
+P:C
+[M:3/4]e^d eB ge | fa fB ^df | e^d eB ge | fa fB ^df | [M:2/4] ef de ||
+P:D
+|: [M:3/4] cd BA eA | cd BA eA | Bc B=G dG | Bd B=G dG |
+cd BA eA | cd BA eA | !coda!GA Bc (3dBG |[1 A2 A2 ed :|[2 A2 A2 E2 !D.S.! |] 
+P:E (coda)
+!coda! GA Bc (3dBG | !fermata!A4|]
+`}
 ]
 }
