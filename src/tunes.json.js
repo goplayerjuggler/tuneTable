f8a331e1ceaebaf0e8e463b7ccc5a2b690f88e1c
Malcolm Schonfield <malcolm_schonfield@yahoo.fr>
2025-10-02 18:41:03 +0200

    Update tunes.json.js
    kringelleken

--- src/tunes.json.js
+++ src/tunes.json.js
@@ -688,6 +688,23 @@ cF/F/F cFdF cBcd cBAc | BGG/G/G DGBG DGdG eGdG  ||
 P:Ⅱ
 F3e fdcA F3e fdcA|d~g3 b~g3 d~g3 b~g3 |
 [M:3/2]dgeg ^fdcA G2gf | [M:4/2] egce ^fdaf gbag fgfd!D.C.!|]
-`]}
+`]}, {abc:`X:1
+T:Kringelleken
+Z:Malcolm Schonfield%230427,250325
+N:On the recording, it’s played “ABABA2BC”
+R:Polska?
+O:Sweden
+D:Per Gudmundson & Bengan Janson - Hjeltamôs
+F:https://music.youtube.com/watch?v=m8mfkFcY17Q
+M:3/4
+Q:1/4=140
+L:1/8
+K:D
+P:ABABA2BC
+[P:A]d2 Pce d2 | eg fe d2 | eg fa/f/ [1,3 dd | ed BG A2 :| [2 de | cd d3A :| [4 de | cd d3f ||
+|: [P:B]ag bf af/e/ | f>a f2 df | ag bf af/e/ | f>a f2 df | eg fe df/e/ | ed BG A2 | 
+a>g bf af/e/ | f>a f2 df | eg fe de |[1 cd d3f :| [2 c<d d3"<("">)"A !D.C.! |]
+[P:C (la dernière fois)]|: d2 ce d2 | eg fe d2 | eg fa/f/ [1 dd | ed BG A2 :| [2 de | cd !fermata!d4 |]
+`}
 ]
 }
