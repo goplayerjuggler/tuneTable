987237ca409214517a6d1bf3be68abc60c6fdf03
Malcolm Schonfield <malcolm_schonfield@yahoo.fr>
2025-10-02 12:21:10 +0200

    Update webpack.config.js
    fix dev mode issue

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,58 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '', // Fix: Set empty string for inline scripts
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.json$/,
-        type: 'json',
-      },
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+  
+  return {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: '',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.json$/,
+          type: 'json',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: 'body',
+        minify: isDevelopment ? false : {
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+      // Only use inline plugin in production
+      ...(isDevelopment ? [] : [
+        new HtmlInlineScriptPlugin({
+          htmlMatchPattern: [/index.html$/],
+          scriptMatchPattern: [/bundle.*.js$/],
+        })
+      ])
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-      },
-    }),
-    new HtmlInlineScriptPlugin({
-      htmlMatchPattern: [/index.html$/],
-      scriptMatchPattern: [/bundle.*.js$/],
-    })
-  ],
-  devServer: {
-    static: './dist',
-    port: 8080,
-  },
+    devServer: {
+      static: './dist',
+      port: 8080,
+      hot: true,
+      // Don't watch dist folder
+      watchFiles: {
+        paths: ['src/**/*'],
+        options: {
+          ignored: ['**/node_modules/**', '**/dist/**']
+        }
+      }
+    },
+  };
 };
