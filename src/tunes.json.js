0b3787f5d47833422dc425d9d005e660805344b1
SCHONFIELD Malcolm <Malcolm.SCHONFIELD@oecd.org>
2025-10-30 13:34:39 +0100

    Update tunes.json.js

--- src/tunes.json.js
+++ src/tunes.json.js
@@ -186,8 +186,8 @@ N: transposes it up a third.
 M: 4/2
 L: 1/8
 K: A
-|:"A"c2-!>!cc .c2c2 "D"BcBA F2 E"<("">)"F|"A"A2-!>!Ac e2 ce "D"f2ec "E"B2 AB|
-"A"c2-cc .c2c2 "D"BcBA F2 E"<("">)"F|"A"A2-Ac efec  "E"B2 .A2 [1 "A"A2-AB:| [2 "A"A2-Ac||
+|:"F#m"c2-!>!cc .c2c2 "D"BcBA F2 E"<("">)"F|"A"A2-!>!Ac e2 ce "D"f2ec "E"B2 AB|
+"F#m"c2-cc .c2c2 "D"BcBA F2 E"<("">)"F|"A"A2-Ac efec  "E"B2 .A2 [1 "A"A2-AB:| [2 "A"A2-Ac||
 |:"A"e2-ee .e2 e2 BccB ABce|"D"f2-ff .f2 f2 "A"BccB ABcd|
 e2-ee .e2 e2 BccB ABce     |"D"a2 af "E"ecBe  "A"c2 .A2 [1 A2 Ac:| [2 A2 AB||
 `,
