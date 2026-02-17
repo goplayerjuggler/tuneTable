16cb0ce3514e5e8c2497fa209e1e6a9099c0b04d
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-02-17 20:18:08 +0100

    Paddy Fahey’s No. 2

--- src/tunes.json.js
+++ src/tunes.json.js
@@ -2062,21 +2062,24 @@ K:Dmajor
 			theSessionId: 1822
 		},
 		{
+			aka: "Paddy Fahey’s",
 			abc: `X:1
-T:Paddy Fahey's
+T:Paddy Fahey’s No. 2
 C:Paddy Fahey
 R:jig
 L:1/8
-M:6/8
+M:12/8
 N:Imported into *tuneTable* on 2025-10-25,
 N:from https://thesession.org/tunes/2667#setting2667
 N:Setting entered in thesession by user “carrie” on 2004-03-15
+N:*abc-tools: convert to M:12/8*
 K:Cmajor
-|:c3 dcB|cGE CEG|F3 DEF|GAB ced|
- c3 dcB|cGE CEG|F3 DGF|ECC C3:|
- |:c3 g^fg|ea^f gec|A2 f fef|Adf afd|
- c3 g^fg|ea^f g2 a|gec fdB|cGE C3:|`,
-			theSessionId: 2667
+|:c3 dcB cGE CEG|F3 DEF GAB ced|
+c3 dcB cGE CEG|F3 DGF ECC C3:|
+|:c3 g^fg eaf gec|A2 f fef Adf afd|
+c3 g^fg eaf g2 a|gec fdB cGE C3:|`,
+			theSessionId: 2667,
+			parts: "AABB"
 		},
 		{
 			abc: `X:1
