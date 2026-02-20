42c958de0a6e019d4828ab7271fc2a4392dc82cb
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-02-20 18:12:45 +0100

    tunes

--- src/tunes.json.js
+++ src/tunes.json.js
@@ -1,5 +1,5 @@
 export default {
-	lastUpdate: "2026-02-19",
+	lastUpdate: "2026-02-20",
 	setLists: [
 		{
 			id: "1771406869982-ukwxx",
@@ -407,7 +407,8 @@ K:Gmajor
 		},
 		{
 			groups: "ALORA",
-			abc: `X: 1
+			abc: [
+				`X: 1
 T: Top The Candle
 S: Frank & Pat Reilly, fiddles
 N:1. A hop jig played in the good old way by two County
@@ -417,8 +418,6 @@ N:this in 9/8 would be wrong here, IMO.
 N:You could perhaps write that as “♪♪ ≠ ♩♪”.
 N:In the recording this was played a few moments before Do it
 N:Fair.
-N:3. (2024-11-08) For this version I added chords and
-N:transposed it from C to G.
 R: hop jig
 F:https://www.itma.ie/playlists/furls-leitrim-performers/?track=23
 D:ITMA – Furls of Music – Leitrim Performers
@@ -427,13 +426,51 @@ Z:abc-transcription Malcolm Schonfield% 2024-08-22, 2024-08-25
 Z:abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
 M: 3/4
 L: 1/8
-K: G
+K: D
 P:Ⅰ
-|:"G"~G2 dG BG|"D"FA2B cA|"G"~G2 dG BG|DG2B [1 AG:| [2 Ad ||
+|:"D"~D2 AD FD|"A"CE2F GE|"D"~D2 AD FD|A,D2F [1 ED:| [2 EA ||
 P:Ⅱ
-"G"~b2 ag fd|"Am"ea2g fd|"C"{d}e2 dc BG|"G"DG2B Ad|
-"G"~b2 ag fd|"Am"ea2g fd|"Am/C"{d}e2 dc BA|"D"GB Ac BA!D.C.!|]
-`
+"D"~f2 ed cA|"Em"Be2d cA|"G"{A}B2 AG FD|"D"A,D2F EA|
+"D"~f2 ed cA|"Em"Be2d cA|"Em/G"{A}B2 AG FE|"A"DF EG FE!D.C.!|]
+`,
+				`X: 1
+T: Top The Candle
+N: 0. This is an older version I posted (in a comment) to thesession on 2024-11-27.
+N: 1. A hop jig played in the good old way by two County
+N: Leitrim fiddlers.
+N: 2. Quavers are played “straight”, and not “swung”; writing
+N: this in 9/8 would be wrong here, IMO.
+N: You could perhaps write that as “𝅘𝅥𝅮𝅘𝅥𝅮 ≠ 𝅘𝅥𝅘𝅥𝅮”.
+N: 3. In the recording this was played a few moments before
+N: Do It Fair.
+N: 4. In the recording it’s played in C; I suspect the fiddles
+N: were tuned one whole tone down from the standard tuning. Here
+N: it’s transposed up one tone so as to be more accessible to
+N: fiddlers nowadays.
+N: 5. I give some rough indications about the ornaments/
+N: embellishments with the “footnotes” on the last line. But
+N: there are lots of subtleties of the rhythm, or phrasing, that
+N: I’ve left out.
+R: hop jig
+F:https://www.itma.ie/playlists/furls-leitrim-performers/?track=23
+F:https://s3-eu-west-1.amazonaws.com/audio.itma.ie/Michael_McNamara_Collection/Item_036_ITMA0262643_TC00.00.05.mp3
+S:Frank & Pat Reilly
+D:ITMA – Furls of Music – Leitrim Performers – track #23
+I:abc-charset utf-8
+Z:abc-transcription Malcolm Schonfield% 2024-08-22, 2024-08-25, 2024-11-27
+Z:abc-copyright CC BY-NC 2.0 (https://creativecommons.org/licenses/by-nc/2.0/fr/deed.en)
+M: 3/4
+L: 1/8
+K: D
+P:A
+|:"^(1)"~D2 AD FD|CE2F GE|"^(1)"~D2 AD FD|"^(2)"A,D2F [1 ED:| [2 EA ||
+P:B
+"^(3)"~f2 ed cA|Be2d cA|{A}B2 AG FD|A,D2F EA|
+"^(3)"~f2 ed cA|Be2d cA|{A}B2 AG FE|DF EG FE!D.C.!|]
+"^(1)" (D/"<("">)"E/D) || "^(2)"A,(D LD)Fyy || "^(3)" (f/"<("">)"g/f)`
+			],
+			theSessionId: 10116,
+			theSessionSettingId: 52515
 		},
 		{
 			groups: "ALORA",
@@ -464,11 +501,8 @@ P:Ⅱ
 "D"~f2fd "Bm"fa | "Em"be2d (c/B/A) | "D"f2fd "A"fa |"D"Ad2A (F/E/D)|
 "D"~f2fd "Bm"fa | "G"be2d (c/B/A) | "D"(f/g/f) "(A)"(e/f/e) "Bm"d2 |"D"Ad2A (F/E/D)!D.C.!|]
 `,
-			references: [
-				{
-					notes: "#2 in the (ALORA) Top The Candle set"
-				}
-			]
+			theSessionId: 268,
+			theSessionSettingId: 52517
 		},
 		{
 			abc: `X:1
@@ -8756,6 +8790,7 @@ I:abc-charset utf-8
 N: As far as I can tell from info on Bandcamp, the artist/instrument list here is:
 N: Angus MacKenzie, uillean pipes; Damian Helliwell, mandolin; Gabe McVarish, fiddles; 
 N: Ross Martin, guitar; Griogair Labhruidh, uillean pipes; Jenny Hill: double bass
+N: Waiting to hear from Dàimh before publishing my full transcription.
 R: strathspey
 M: 4/2
 L: 1/8
