d0d520b0e84122968086152e09c7d9941653285d
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-02-24 02:38:30 +0100

    tunes

--- src/tunes.json.js
+++ src/tunes.json.js
@@ -9554,7 +9554,23 @@ M:4/4
 |:A2|d2A2 F2GA BAGF E3F|GAGF E2FG AGFE D2A2|
 d2A2 F2GA BAGF E3F | [M:5/4] GFGA BAFD AGEC D4 D2:|`,
 			theSessionId: 4587,
+			theSessionSettingId: 48618,
 			parts: "AABB",
+			badges: "crooked"
+		},
+		{
+			abc: `X:1%240423
+T:_
+O:France
+R:Mazurka
+N:(2026-02-24) I have the info about this somewhere - will fill 
+N: in the blanks another day!
+M:3/4
+L:1/8
+K:D
+|: fg | [M:4/4] a2a2af de | f2 f2 ef ag | e2 e2 de ge | [M:3/4] f2 de fg |
+[M:4/4]a2a2af de | f2 f2 ef ag | e2 e2 Ad fe | [M:3/4] d2- [1 de :| [2 d2 
+|: Ad | f2 e2 Bc | B2 A2 Ac | e2 eg fe | f2 d2 Ad | f2 e2 Bc | B2 A2 Ac | e2- eg fe | d2- [1 d2 :| [2 de !D.C.! |]`,
 			badges: "crooked"
 		}
 		/*
