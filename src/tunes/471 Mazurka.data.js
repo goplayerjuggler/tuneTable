4a2459ae30b67fe969860ab8e51ac327fe4ea82c
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-03-13 17:16:16 +0100

    _
    revisions after a quick listen

--- src/tunes/471 Mazurka.data.js
+++ src/tunes/471 Mazurka.data.js
@@ -1,17 +1,20 @@
 export default {
-	abc: `X:1%240423
+	abc: `X:1
 T:Mazurka de Louis Jarraud
 O:France; Limousin.
 S: Duo Rivaud Lacouchie (Alexandra Lacouchie, violon; Anne Rivaud, accordéon diatonique)
-H: Jouée au violon par Louis Jarraud, né en 1910, de La Croisille-su-Briance (87), recueillie en 1982 par F. Etay
-N: [discogs](https://www.discogs.com/release/11152134-Duo-Rivaud-Lacouchie-Ordich)
-D:Ordich! (2016?)
+H: Info copiée depuis l’album : « Jouée au violon par Louis Jarraud, né en 1910, de La Croisille-sur-Briance (87), recueillie en 1982 par F. Etay »
+N: 1. [discogs](https://www.discogs.com/release/11152134-Duo-Rivaud-Lacouchie-Ordich)
+N: 2. La « (*) » sur la mesure nº 4 renvoie à des variations à la fin.
+D:Ordich! (2016 ?)
 R:Mazurka
-N: Dans l’album, c’est suivi de la Mazurka de Saint-Bonnet 
+N: Dans l’album, suivie de la Mazurka de Saint-Bonnet 
 M:3/4
 L:1/8
 K:D
-fg | [M:4/4] a2a2af de | f2 f2 ef ag | e2 e2 de ge | [M:3/4] f2 de fg | [M:4/4]a2a2af de | f2 f2 ef ag | e2 e2 Ad fe | [M:3/4] d4
-Ad | f2 e2 Bc | B2 A2 Ac | e2 eg fe | f2 d2 Ad | f2 e2 Bc | B2 A2 Ac | e2- eg fe | d2 de !D.C.! |]`,
-	tags: "crooked"
+fg | [M:4/4] .a2a2af de | .f2 f2 ef ag | e2 e2 de ge | [M:3/4] "^(*)"f2 d2 fg | [M:4/4]a2a2af de | f2 f2 ef ag | e2 e2 Ad fe | [M:3/4] d4
+Ad | f2 e2 Bc | B2 A2 Ac | e2 e2 de | f2 d2 Ad | f2 e2 Bc | B2 A2 Ac | e2 eg fe | d2 de !D.C.! |]
+"^(*)"f2de fg ||"^(*)"fe de fg ||`,
+	tags: "crooked",
+	parts: "AB"
 };
