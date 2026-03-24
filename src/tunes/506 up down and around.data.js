c38aadd622b135f85cbbc103d81ff552a9579473
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-03-24 01:44:58 +0100

    tunes

---
+++ src/tunes/506 up down and around.data.js
@@ -0,0 +1,22 @@
+export default {
+	groups: "su",
+	parts: "AABB",
+	abc: [
+		`X:1
+T:Far Away
+C:Pete Jung
+R:waltz
+L:1/8
+M:3/4
+N:Imported into *tuneTable* on 2026-03-24,
+N:from https://thesession.org/tunes/187#setting12835
+N:Setting entered in thesession by user “ceolachan” on 2007-07-27
+K:Dmajor
+|:B2 BF (3Bcd|cA F2 FA|B2 BF Be|c3 c dc|
+ B2 BG (3Bcd|cB A2 de|fe dc BA|1 B2 BA FA:|B2 Bc de||
+ |:f3 d fa|ec A2 de|f3 d fa|e4 de|
+ fd Be cA|dB Gd Be|F2 B2 A2|1 B2 Bc de:|2 B2 BA FA||`
+	],
+	theSessionId: 187,
+	theSessionSettingId: 12835
+};
