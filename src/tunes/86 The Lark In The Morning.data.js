d3af7bd7aa1dbc400898fff781ccb35f30aeca45
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-03-26 21:13:46 +0100

    add ref
    Cillian Vallely

--- src/tunes/86 The Lark In The Morning.data.js
+++ src/tunes/86 The Lark In The Morning.data.js
@@ -5,18 +5,26 @@ export default {
 T:The Lark In The Morning
 R:jig
 L:1/8
-M:6/8
+M:12/8
 N:Imported into *tuneTable* on 2025-10-25,
 N:from https://thesession.org/tunes/62#setting62
 N:Setting entered in thesession by user “Jeremy” on 2001-05-25
+N:*abc-tools: convert to M:12/8*
 K:Dmajor
-|:AFA AFA|BGB BdB|AFA AFA|fed BdB|
- AFA AFA|BGB BdB|def afe|dBB BdB:|
- |:def afe|bff afe|def afe|dBB BdB|
- def afe|bff afe|g2e f2d|edB BdB:|
- |:dff fef|fef fef|dff fef|edB BdB|
- dff fef|fef def|g2e f2d|edB BdB:|
- |:Add fdd|edd fdd|Add fdd|edB BdB|
- Add fdd|edB def|g2e f2d|edB BdB:|`,
-	theSessionId: 62
+|:AFA AFA BGB BdB|AFA AFA fed BdB|
+AFA AFA BGB BdB|def afe dBB BdB:|
+|:def afe bff afe|def afe dBB BdB|
+def afe bff afe|g2e f2d edB BdB:|
+|:dff fef fef fef|dff fef edB BdB|
+dff fef fef def|g2e f2d edB BdB:|
+|:Add fdd edd fdd|Add fdd edB BdB|
+Add fdd edB def|g2e f2d edB BdB:|`,
+	theSessionId: 62,
+	references: [
+		{
+			url: "https://www.youtube.com/watch?v=yoiB1-Ubn2I",
+			artists: "Cillian Vallely, uillean pipes; Alan Murray, guitar",
+			notes: `The Lark In The Morning/Throw Away The Keys/Kiss The Maid Behind The Barrel (1 jig + 2 reels; 2012)`
+		}
+	]
 };
