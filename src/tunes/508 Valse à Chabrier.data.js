cd9221b8c0f45d766368928d46806bd0d9817d09
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-03-26 10:51:29 +0100

    Valse à Chabrier

---
+++ src/tunes/508 Valse à Chabrier.data.js
@@ -0,0 +1,32 @@
+export default {
+	abc: `X:1
+T:Valse à Chabrier
+R:Waltz
+O:France;Auvergne;Artense
+S:Clémence Cognet, violon; Noëllie Nioulou, violoncelle
+A:Les poufs à cordes / La Trotteuse (2013)
+Z:abc-transcription Malcolm Schonfield
+Z:abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+N:1. Cette valse magnifique nous a été transmise par 
+N:Antonin Chabrier, violonneux de l’Artense. Des enregistrements de lui ont été
+N:publiés en 1975 et 1980.
+N:2. Les accords que j’ai rajoutés ne sont qu’une esquisse harmonique. 
+N:Au lieu de C7 on pourrait très bien mettre autre chose, 
+N:genre des accords ouverts; ou rien !
+N:3. J’ai donné quelques indications sommaires sur les phrasés, mais
+N:il vaut mieux écouter ça si on peut !
+H: Il y a quelque chose de magique ici…
+M:3/4
+L:1/4
+K:C mixo
+"C"G~AG|cB~A|Gcd|e-!>!~e/d/e/f/|g.ag|"C7"fd/e/d/e/|fg/f/e/d/|"C"e>dc/A/|
+GAG|cB~A|Gcd|e-!>!~e/d/e/f/|g.ag|"C7"fd/e/d/e/|fg/f/e/d/|[1c3:|[2c2.G||
+"C"c>Bc/d/|edc|e>de/f/|g3|Ggf|e/f/g/f/e/f/|"C7"de/f/e/d/|"C"e/d/c/G/e/d/|
+c>Bc/d/|edc|e>de/f/|g3|G~g~f|e/f/g/f/e/f/|"C7"de/f/e/d/|c3||
+Gga|g3|e/f/e/d/c/d/|e3|e/f/e/d/e/f/|g.fe|"C7"de/f/e/d/|"C"e>dc/A/|
+Gga|g3|e/f/e/d/c/d/|e3|e/f/e/d/e/f/|g.fe|"C7"de/f/e/d/|"C"c3|]
+`,
+	parts: "AABC",
+	groups: "alora",
+	contourShift: -1
+};
