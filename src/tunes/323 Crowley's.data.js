02410e93516861fb7b55d3c0550dd08cdec8a03f
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-03-30 10:46:18 +0200

    _
    ref for crowleys & add the no.2

--- src/tunes/323 Crowley's.data.js
+++ src/tunes/323 Crowley's.data.js
@@ -7,13 +7,20 @@ R: reel
 M: 4/4
 L: 1/16
 K: Dmaj
-A3d B2dB AD F/E/D AD F/E/D|A3d B2dB`,
+A3d B2dB ADF/E/D ADF/E/D|A3d B2dB`,
 	theSessionId: 759,
 	references: [
 		{
 			artists: "Joe Burke, accordion; Martin Byrnes, fiddle",
 			url: "https://www.itma.ie/playlists/padraic-mac-mathunas-monthly-picks-december-2024/?track=1",
 			notes: "“Crowley’s reels”. 1973. Poor audio quality"
+		},
+		{
+			artists:
+				"Brendan McGlinchey, fiddle; Johnny Pickering, fiddle; Kevin McElhatton, piano",
+			url: "https://www.itma.ie/playlists/padraic-mac-mathunas-monthly-picks-july-2023/?track=2",
+			notes:
+				"“Crowley's no. 1/Crowley's no. 2”. First tune; starts at 0:19. 1950s or 1960s. Doubled (AABB)."
 		}
 	]
 };
---
+++ src/tunes/509 Crowley's.data.js
@@ -0,0 +1,19 @@
+export default {
+	aka: "Crowley's No.2",
+	parts: "AB",
+	groups: "su",
+	incipit: `X: 1
+T: Crowley's
+R: reel
+M: 4/4
+L: 1/16
+K: Dmaj
+~f2ed edBc dBBA BAFA|~B3d AFDF`,
+	theSessionId: 1180,
+	crossReferences: [
+		{
+			theSessionId: 759,
+			index: 1
+		}
+	]
+};
