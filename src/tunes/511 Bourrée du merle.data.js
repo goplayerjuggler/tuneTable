4586190cd901d26c45a123988312ef1760ad2fea
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-04-02 01:48:16 +0200

    bourée du merle

---
+++ src/tunes/511 Bourrée du merle.data.js
@@ -0,0 +1,57 @@
+export default {
+	aka: "Lo merle",
+	parts: "AABB’",
+	abc: `
+X:1
+T:Bourrée du merle
+R:Bourrée
+O:France;Limousin;Corrèze.
+S:Clémence Cognet, violon ; Noëllie Nioulou, violoncelle
+D:Les poufs à cordes / La Trotteuse (2013)
+Z:abc-transcription Malcolm Schonfield
+Z:abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+M:3/8
+L:1/8
+N:0. Unfortunately this fantastic album doesn’t seem to be available online.
+N:1. I’m getting a sort of phantom anacrusis (pickup) effect here
+N:with the “A|de2” fragment - for me the A part really starts with 
+N:the “a>ba”. So it feels like a “crooked” tune, even though after 
+N:writing it down I can see that it isn’t! 
+N: With this pickup effect, there's a stress on bar nb 2 rather than on
+N: bar nb 1, which creates a sort of a tension and makes the tune
+N: particularly interesting/special.
+N:2. Many years ago I was taught this tune by the fiddler Basile Brémaud, 
+N: in a workshop. I find it a tough tune to play nicely.
+N:3. There’s another, different bourrée à trois temps that goes by the
+N: same name.
+N:4. I put this as AABB’ but it could also be written AABC, or AAB with 
+N:the B part twice as long as the A.
+N:5. From some notes on the album, this tune comes down to us via the 
+N: fiddler and singer Léon Peyrat (1905-1988). 
+K:D
+A|de2|a>ba|ga/g/e/f/|ga2|b/a/gf|e>dc|
+de2|a>ba|ga/g/e/f/|ga2|b/a/gf|e2d||
+cA2|Adc|ded|cA2|Adc|d2g|
+fe2|Adc|def|ga2|g/a/g/f/e|d2|]`,
+	references: [
+		{
+			artists: "Léon Peyrat, voice",
+			url: "https://youtu.be/D10CdDoTXE0?t=230",
+			notes:
+				"A different setting of this tune as a song in Occitan; a very short extract but still recognisable and interesting."
+		},
+		{
+			artists: "Léon Peyrat, voice & fiddle",
+			url: "https://youtu.be/GJCjhLQbtsU",
+			notes:
+				"The first 82 seconds here have L Peyret first playing this on the fiddle, and then singing it."
+		},
+		{
+			artists: "Emilyn Stam, fiddle; John David Williams, clarinet",
+			url: "https://emilynandjohn.bandcamp.com/track/bourr-es-les-forgerons-aiment-le-vin-lo-merle",
+			notes: `Bourrées - Les forgerons aiment le vin / Lo merle
+From 01:23 onwards. The B part here is slightly different from the score I wrote.`,
+			album: "Iris"
+		}
+	]
+};
