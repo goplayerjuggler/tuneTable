b222144cbbd59ddbba204e1a184a13ea0471c703
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-04-02 01:53:05 +0200

    The Ballykeale

---
+++ src/tunes/513 The Ballykeale.data.js
@@ -0,0 +1,28 @@
+export default {
+	parts: "AABB",
+	abc: `X:1
+T:The Ballykeale
+R:jig
+L:1/8
+M:12/8
+N:This is the version off the Bright and Early album.
+N:---
+N:Imported into *tuneTable* on 2026-04-01,
+N:from https://thesession.org/tunes/2935#setting42250
+N:Setting entered in thesession by user “Trr67” on 2021-10-31
+N:*abc-tools: convert to M:12/8*
+K:Gmixolydian
+dc|:AFA Adc AFA ABc|BGG dGG BGB dcB|
+[1AFA Adc AFA Aga|bag ^fdc BGB dcB:|
+[2 AFA dAc A/B/cA Aga|bag ^fdc BGG GBc||
+|:dgg gag ^fde =fdc|dgg bag ^fd^c def|
+g3 bag ^fde =fga|bag ^fdc[1 BGG GBc:|2 BGB dcB||`,
+	theSessionId: 2935,
+	theSessionSettingId: 42250,
+	crossReferences: [
+		{
+			ttId: 512
+		}
+	],
+	ttId: 513
+};
