8555c4c9171d63d5b8444a95a20b4501f7164da3
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-04-02 01:52:26 +0200

    ram[pb]ling ptichfork
    refactor & tweaks

---
+++ src/tunes/512 Tommy Potts' Rambling Pitchfork.data.js
@@ -0,0 +1,39 @@
+export default {
+	parts: "AABBC",
+	ttId: 512,
+	abc: `
+X: 1
+T:The Rambling Pitchfork
+%T: Tommy Potts' Rambling Pitchfork
+%C: Tommy Potts
+%T: As played by Paddy O'Brien and Nathan Gourley
+S: Paddy O’Brien, accordion; Nathan Gourley, fiddle
+D: Bright And Early
+N: The Ballykeale / The Rambling Pitchfork 
+F: https://music.youtube.com/watch?v=FqNstTlVggE&t=64
+F: https://open.spotify.com/track/05m3z6WmKqWGd4xluoujTT
+Z: abc-transcription Malcolm Schonfield% 2024-05-17,24
+Z: abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+N: Paddy O’Brien, button accordion; Nathan Gourley, fiddle; Dáithí Sproule, guitar
+R: jig
+M: 12/8
+L: 1/8
+K: Dmin
+E |:[P:A] F2F AFF cFF AGF | G2A Bde fed cAG | 
+FDD ADD CDD A,DF | G2A BAG [1 AFD DCD :| [2 AFC D2d ||
+|:[P:B] d^cd ^fed e^cA =B/c/de | d^cd e^fg a2^f gfe |
+d2e =fed e^cA E^FG | ^FGA BAG A^ce [1 dcA:| [2 dag ||
+[P:C] fed ecA dAc AFD | G2A Bde fed cAG |
+FDD ADD CDD A,DF | G2A BAG AFC D2 !D.C.! |]`,
+
+	references: [
+		{
+			notes: `See also [Jimmy Power’s setting](ttId=73)`
+		}
+	],
+	crossReferences: [
+		{
+			ttId: 513
+		}
+	]
+};
--- src/tunes/73 Tommy Potts' Rampling Pitchfork.data.js
+++ src/tunes/73 Tommy Potts' Rampling Pitchfork.data.js
@@ -1,9 +1,8 @@
 export default {
-	abc: [
-		`
+	abc: `
 X: 1
 T: Tommy Potts' Rampling Pitchfork
-T: As played by Jimmy Power
+%T: As played by Jimmy Power
 %C: Tommy Potts
 S: Jimmy Power, fiddle
 D: Irish Fiddle Player
@@ -16,40 +15,24 @@ R: jig
 M: 12/8
 L: 1/8
 K: D minor
-[P:A] D2SE || FEF GFG  AGA dA^F | G/G/^FG A2g  fed cAG |
+[P:A] D2SE || FEF GFG  AGA dA^F | G/G/^FG A2g  =fed cAG |
 ^FDD ADD  CDD A,DD | ([GG,3]FG) ABG AFD D2E | 
-FEF [GG,]FG AGA dA^F |  G/G/^FG A2g fed cAG |
-^FDD ADD CDD A,DD | ([GG,3]FG)  A/A/BG AFD
- [P:B] D2A |:  d2g fed ecA ABc |  d/d/de fed  f/g/af gec |
+FEF [GG,]FG AGA dA^F |  G/G/^FG A2g =fed cAG |
+^FDD ADD CDD A,DD | ([GG,3]FG)  A/A/BG AFD D2A |
+ [P:B] |:  d2g fed ecA ABc |  d/d/de fed  f/g/af gec |
 d2g fed ecA EFG |  F/F/GA ABG AFD [1 [A2D2]A:| 
 [2 [P:C] D2E |: FEF AFF cFF AFF | GFG A2g fed cAG |
 ^FDD ADD CDD A,DD | GFG ABG AFD [1D2E:| 
 [2 [P:D] D2e |: (fef) ecA BAB AFD | (GFG) A2g fed cAG |
 ^FDD ADD CDD A,DD | (GFG) ABG AFD [1 D2e :| [2 D2!D.S.! |]
 `,
-		`
-X: 3
-T:The Rambling Pitchfork
-T: Tommy Potts' Rambling Pitchfork
-%C: Tommy Potts
-T: As played by Paddy O'Brien and Nathan Gourley
-S: Paddy O’Brien, accordion; Nathan Gourley, fiddle
-D: Bright And Early - The Ballykeale / The Rambling Pitchfork 
-F: https://music.youtube.com/watch?v=FqNstTlVggE&t=64
-F: https://open.spotify.com/track/05m3z6WmKqWGd4xluoujTT
-Z: abc-transcription Malcolm Schonfield% 2024-05-17,24
-Z: abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
-N: Paddy O’Brien, button accordion; Nathan Gourley, fiddle; Dáithí Sproule, guitar
-R: jig
-M: 12/8
-L: 1/8
-K: Dmin
-E |:[P:A] F2F AFF cFF AGF | G2A Bde fed cAG | 
-FDD ADD CDD A,DF | G2A BAG [1 AFD DCD :| [2 AFC D2d ||
-|:[P:B] d^cd ^fed e^cA =B/c/de | d^cd e^fg a2^f gfe |
-d2e =fed e^cA E^FG | ^FGA BAG A^ce [1 dcA:| [2 dag ||
-[P:C] fed ecA dAc AFD | G2A Bde fed cAG |
-FDD ADD CDD A,3 | G2A BAG AFC D2 !D.C.! |]`
+	theSessionId: 2329,
+	references: [
+		{
+			notes: `1. The session tune, referenced in thessession.org (ID 2329), is IMO a different tune, albeit with a close family relationship to this one. 
+2. I also give here [another Tommy Potts-inspired setting in three parts (Paddy O’Brien & Nathan Gourley)](ttId=512).`
+		}
 	],
-	theSessionId: 2329
+	parts: "AABBCCDD",
+	ttId: 73
 };
