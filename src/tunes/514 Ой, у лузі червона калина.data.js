6d0b910e3a58db1dc34b44d0b420ab6182a28080
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-04-02 09:01:13 +0200

    Ой, у лузі червона калина

---
+++ src/tunes/514 Ой, у лузі червона калина.data.js
@@ -0,0 +1,39 @@
+export default {
+	parts: "ABB",
+	groups: "alora",
+	ttId: 513,
+	abc: `
+X:1
+T:Ой, у лузі червона калина
+O:Ukraine
+R:Song
+S:Eileen, voice
+F:https://www.youtube.com/watch?v=ZztmQsSAqfo
+Z:abc-transcription Malcolm Schonfield
+Z:abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+M:2/2
+L:1/8
+K:Gmin
+G2G2 B3G | ABAG ^F2D2|G3A B2A2 | G4z4|
+w: Ой, у лу-зі | чер-во-на ка-ли-на | по-хи-ли-ла-ся
+G2G2 c3G | BAG^F G2D2|[M:3/2]G3A B2A2 (G2F)z||
+w:Чо-гось на-ша сла-вна У-кра-ї-на за-жу-ри-ла-ся
+|:[M:2/2]B2d2f3=e|[M:3/2]fgfe dcde dcBA |
+w:А ми ту-ю чер-во-ну ка-ли-ну пі-ді-й-  * ме-мо
+[M:2/2]G2B2 d3c|dedc BAGB |1 [M:5/4]d2c2 BA G2 F2 :|2 [M:7/4]d2c2 BA G4 z4 |]
+w:А ми на-шу сла-вну У-кра-ї-ну, гей, гей, ро-зве-се-ли-мо * ро-зве-се-ли-мо
+
+X:2
+T:Ой, у лузі червона калина
+Z:abc-transcription Malcolm Schonfield
+Z:abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+N:1. I added some chords - just a rough outline.
+N:2. More work is needed here to transcribe the backing vocal line.
+M:2/2
+L:1/8
+K:Gmin
+"Gm"G2G2 B3G | "D"ABAG ^F2D2|"Gm"G3A "Dm"B2A2 | "Gm"G4z4|
+"Gm"G2G2 "Cm""Gm"c3G | "D"BAG^F "Gm"G2D2|[M:3/2]"Gm"G3A "Dm"B2A2 "F"(G2F)z||
+|:[M:2/2]"Bb"B2d2f3=e|[M:3/2]"Bb"fgfe dcde "D"dcBA |
+[M:2/2]"Gm"G2B2 d3c|"Gm"dedc BAGB |1 [M:5/4]"Cm"d2c2 "Dm/F"BA "Gm"G2 F2 :|2 [M:7/4]"Cm"d2c2 "Dm/F"BA "Gm"G4 z4 |]`
+};
