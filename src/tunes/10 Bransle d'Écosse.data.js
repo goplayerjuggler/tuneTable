3cf6c4dd5bf37cd4a87414e25c52700343ca482f
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-04-08 20:26:46 +0200

    fix - 5/2 meter at end

--- src/tunes/10 Bransle d'Écosse.data.js
+++ src/tunes/10 Bransle d'Écosse.data.js
@@ -10,7 +10,7 @@ K: E dorian
 |: "Em"EFGE "D"FGA2  "Bm"AGFE "Em"GAB2  "Em"BdcB "A"AG"Bm"FE |
 "Em"EFGE "D"FGA2  "Bm"AGFE "Em"GAB2  "A"AG"Bm"FE "Bm"FD"Em"E2 :|
 |: "Em"B2Bc "A"dedc  "Em".B2B2 "D"AGFE  "Em"B2AG "Bm"F2E2 |
-[M:5/4] "Em"B2Bc "A"dedc  "Em".B2B2 "A"AG"Bm"FE "B"F<D"Em"E2 :|`,
+[M:5/2] "Em"B2Bc "A"dedc  "Em".B2B2 "A"AG"Bm"FE "B"F<D"Em"E2 :|`,
 	incipit: `X:1
 M:6/2
 L:1/8
