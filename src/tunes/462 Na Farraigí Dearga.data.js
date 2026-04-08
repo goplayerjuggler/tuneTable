275de45d26152864d903922f4993794bda88ce76
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-04-08 08:41:17 +0200

    highland?

--- src/tunes/462 Na Farraigí Dearga.data.js
+++ src/tunes/462 Na Farraigí Dearga.data.js
@@ -4,16 +4,18 @@ export default {
 X:1
 T: Na Farraigí Dearga
 T: The Seas They Ran Red with Blood
-R:Hornpipe
+R:Highland
 D: Decade (2019)
 S:Fidil
 F:https://raelachrecords.bandcamp.com/track/na-farraigi-dearga-the-seas-they-ran-red-with-blood-descriptive-piece
 M:4/2
 N: 1. Mostly swung throughout – so in general “♪♪=♩♪”
-N: 2. It's a lovely tune; remarkable.
-N: 3. I put this as a hornpipe as that seems to me to be the best fit; on the
-N: album it's called a descriptive piece.
-N: 4. Waiting to hear from Raelach Records before publishing my full transcription.
+N: 2. It's a fantastic, lovely tune; remarkable.
+N: 3. After some hesitation, I’ve put this as a highland.
+N: I initially went for “hornpipe”. On the album it's called 
+N: a descriptive piece.
+N: 4. Waiting to hear from Raelach Records before publishing 
+N: my full transcription.
 L:1/8
 K:Dm
 A,DDE F2AG FD (3EDC B,CA,B, | G,2 (3G,B,D`,
