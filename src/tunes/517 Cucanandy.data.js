c9a7979196378390636a0d7cfe6aecfae1af64de
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-04-29 13:34:36 +0200

    tunes
    add refs; tweaks

--- src/tunes/517 Cucanandy.data.js
+++ src/tunes/517 Cucanandy.data.js
@@ -9,6 +9,14 @@ L:1/8
 K: G
 |:"G"B3 B2 A G2 A|B2 d d2 B d3|B3 B2 A G2 A|"Em"B2 e e2 d e3:|
 "Em"e2 f g2 f g3|"D"B2 d d2 B d3|"Em"e2 f g2 f g2 d|"Em"B2 e e2 d e3|
-"Em"e2 f g2 f g3|"D"B2 d d2 B d2"<("">)"g|"Em"e2 B B2 A G2 A|"Em"B2 e e2 d e2"<("">)"d||`,
-	theSessionId: 1374
+"Em"e2 f g2 f g3|"D"B2 d d2 B d2"<("">)"g|"Em"e2 B B2 A G2 A|"Em"B2 e e2 d \\ %"_to end this set, finish on a held note here with a backing C chord"
+e2"<("">)"d||`,
+	theSessionId: 1374,
+	crossReferences: [
+		{
+			theSessionId: 1340,
+			index: 0
+			// The Boys of Ballosodare / James Kelly's / Cucanandy - bodhrán festival
+		}
+	]
 };
--- src/tunes/522 James Kelly's.data.js
+++ src/tunes/522 James Kelly's.data.js
@@ -10,5 +10,12 @@ L: 1/8
 K: Dmaj
 |:"D"F2A A2F A2F|A3- A2A Bcd|F2A A2F A2d|"G"e2f e2d B2A:|
 |:"D"f2a a2f d3|"A"e2f e2d B2A|"D"f2d e2c d3|"G"B2=c B2A FED:|`,
-	theSessionId: 1492
+	theSessionId: 1492,
+	crossReferences: [
+		{
+			theSessionId: 1340,
+			index: 0
+			// The Boys of Ballosodare / James Kelly's / Cucanandy - bodhrán festival
+		}
+	]
 };
