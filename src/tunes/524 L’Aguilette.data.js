5ac5bfb33255dffa31ed18ca203f9f49d131850b
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-04-30 02:43:26 +0200

    Arthur - L'Aguilette

---
+++ src/tunes/524 L’Aguilette.data.js
@@ -0,0 +1,23 @@
+export default {
+	parts: "AA’B",
+	groups: "su",
+	ttId: 524,
+	incipit: `X:1
+T:L’Aguilette
+C:Fabio Colussi
+O: Italy
+D: The Northern Concertina
+S:Jason O’Rourke, concertina; Teresa Clarke, piano; Tim Edey, guitar; Seonaid Murray, tenor saxophone; Stevie Dunne, guitar
+F:https://jasonorourke.bandcamp.com/track/laguilette-arthur
+N: * I made a complete score of this in 2018, but as I wasn’t succesful in getting in touch with 
+N: Fabio Colussi, to get his permission, I prefer not to publish it. Pity, but c’est la vie…
+N: It’s a lovely tune that’s beautifully played; highly recommended.
+N: * L’Aguilette / [Arthur](ttId=523)
+R: Mazurka
+M: 3/4
+L: 1/8
+Q: 1/4=60 
+K: Amin
+cB A2 e2 | c3  A cA | d6-| d3B de |
+`
+};
