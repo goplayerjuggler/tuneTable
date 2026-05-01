170f328b1954fe20d93895c7af0cc1577e9bb362
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-01 02:08:08 +0200

    tunes
    une valse des roses

---
+++ src/tunes/522 Valse des rose.data.js
@@ -0,0 +1,14 @@
+export default {
+	abc: `X:1
+T:Valse des roses
+O:France
+R:Waltz
+Z:abc-transcription Malcolm Schonfield
+M:3/4
+L:1/4
+K:G
+|: g2d | B>cd | g2d | B>cd | e2d | c2B | [1 A>GA | B2G :| [2 A>BA | G3 ||
+|: B>cd | e2d | B>cd | e2d | e2d | c2B | [1 A>GA | B2G :| [2 A>BA | G3 !D.C.!|]`,
+	ttId: 522,
+	parts: "AB"
+};
