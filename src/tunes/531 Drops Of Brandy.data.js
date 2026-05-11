2545a606ec0faf01259dd7071b64ff378bf6f71a
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-11 16:55:24 +0200

    tunes
    drops of brandy

---
+++ src/tunes/531 Drops Of Brandy.data.js
@@ -0,0 +1,19 @@
+export default {
+	abc: `X:1
+T:The Drops Of Brandy
+R:slip jig
+L:1/8
+M:9/8
+N:Imported into *tuneTable* on 2026-05-11,
+N:from https://thesession.org/tunes/388#setting13220
+N:Setting entered in thesession by user “ceolachan” on 2007-09-27
+K:D
+|:ABA FDF FDF|ABA FDF GFE|
+ ABA FDF FDF|GFG E2 F GFE:|
+ |:DFA dAF dAF|DFA dAF GFE|
+ [1 DFA dAF dAF|GFG E2 F GFE:|
+ [2 DFA dfe dAF|GFG E2 F GFE||`,
+	theSessionId: 388,
+	parts: "AABB’",
+	theSessionSettingId: 13220
+};
