1510ca423aca2612deb288e00aabaf6a51bcbfc3
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-11 02:20:04 +0200

    Spel-Görans vals

---
+++ src/tunes/529 Spel-Görans vals.data.js
@@ -0,0 +1,14 @@
+export default {
+	abc: `X:1
+T:Spel-Görans vals
+O:Sweden;Blekinge.
+S:Emelie Waldken, nyckelharpa
+F:https://www.youtube.com/watch?v=HSU86cDzmF4
+N:ABC score copied from [folkwiki.se](http://www.folkwiki.se/Musik/2816)
+M:3/4
+L:1/8
+K:Gm
+R:Waltz
+D2| "Gm"D2 GG G2 | "D"^F2 AA A2 | "Gm"B2 dB AG | "D"G^F A^F D2 | "Gm"DG GG G2 | "D"^FA AA A2 |"Gm" B2 dB "D"A^F | "Gm"G4  :|
+AB | "F"c2 Bc dB | "Bb"F4 AB | "F"c2 Bc dB | "Gm"G4 D2 | "Gm"D2 GG GA | B4 AG | "D"G2 ^FG A^F | "G"G4 :|`
+};
