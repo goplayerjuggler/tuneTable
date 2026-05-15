dbd2774476bfeec2fc4ee19dd6f3329f89405985
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-15 12:16:05 +0200

    tunes
    scores for the 3 polkas

--- src/tunes/386 John Brosnan's.data.js
+++ src/tunes/386 John Brosnan's.data.js
@@ -1,14 +1,25 @@
 export default {
 	groups: "alora",
-	incipit: `X: 1
-T: John Brosnan's
-R: polka
-M: 4/4
-L: 1/8
-K: Dmaj
-D>E DB AF FB|AF DE FEEF/E/|`,
-	parts: "AABBCC",
+	abc: `X:1
+T:John Brosnan's
+R:polka
+L:1/8
+M:4/4
+N:Imported into *tuneTable* on 2026-05-15,
+N:from https://thesession.org/tunes/3835#setting16763
+N:Setting entered in thesession by user “ceolachan” on 2008-10-20
+N:*abc-tools: convert to M:4/4*
+N:(edited after importing)
+K:D
+|:A,|D2 DB AF FB|AF D>E FE E2|
+D>C DB AF FB|AF EC D2- D:|
+|:c|d2 cA BA FD|de/d/ cA B2 Bc|
+d2 c2 BA FD|E>^D EF =D2- D:|
+|:A|d2 e2 f2- fe|d>c Bc d2- dA|
+B2 AF Bd/B/ AF|AF EC D2- D:|`,
 	theSessionId: 3835,
+	theSessionSettingId: 16763,
+	parts: "AABBCC",
 	references: [
 		{
 			url: "https://jasonorourke.bandcamp.com/track/john-brosnans-the-frozen-mouse-polka-timmy-mccarthys",
--- src/tunes/388 Neilí's.data.js
+++ src/tunes/388 Neilí's.data.js
@@ -1,22 +1,38 @@
 export default {
 	groups: "alora",
-	incipit: `X: 5
-T: Neilí's
-R: polka
-M: 4/4
-L: 1/8
-K: Amix
-ed cA ed c2|AA/c/ ef ge d2|`,
+	abc: `X:1
+T:Neilí’s
+R:polka
+L:1/8
+M:4/4
+N:with chords
+N:---
+N:Imported into *tuneTable* on 2026-05-15,
+N:from https://thesession.org/tunes/7386#setting56758
+N:Setting entered in thesession by user “rogovath” on 2026-03-09
+N:*abc-tools: convert to M:4/4*
+K:Amixolydian
+|:"A"ed cA ed cA | Ac/d/ ef "G"ge "D/F#"d2 |
+"A"ed cA ed cA | Ac/d/ ef "G"ge "D/F#"d2:|
+|:"A"e>f gf ed ce | ef ge ec "D"d2 |
+"A"e>f gf ed cA | Ac/d/ ef ge "D"d2:|
+|:"D"f2 fg "A"ed "A/C#"cA|"D"f2 fg "A"ec "D"d2|
+"D"f2 fg "A"ed "A/C#"cA|Ac/d/ ef ge "D"d2:|`,
 	theSessionId: 7386,
+	theSessionSettingId: 21010,
 	parts: "AABBCC",
 	references: [
 		{
 			url: "https://jasonorourke.bandcamp.com/track/john-brosnans-the-frozen-mouse-polka-timmy-mccarthys",
 			artists: "Jason O’Rourke, concertina; Stevie Dunne, guitar",
 			notes: `[John Brosnan’s](theSessionId=3835) / [The Frozen Mouse](theSessionId=18688) / Timmy McCarthy’s
-Here this polka, Neilí's, is given the name Timmy McCarthy’s.
+Here this polka, Neilí’s, is given the name Timmy McCarthy’s.
 `,
 			album: "The Northern Concertina (2015)"
+		},
+		{
+			notes:
+				"I had trouble once playing this in a session, because people weren’t aware of the three-part version and played the two-part one instead, which clashed with me playing the three-part one. Note to myself: if you want to play the three part version in a session, it’s probably best to let everyone know beforehand!"
 		}
 	]
 };
