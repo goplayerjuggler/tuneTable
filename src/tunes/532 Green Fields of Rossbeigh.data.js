eb408c964936d27ae2e44b3e67d486536719d32d
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-15 12:32:02 +0200

    tunes
    green fields of rossbeigh

---
+++ src/tunes/532 Green Fields of Rossbeigh.data.js
@@ -0,0 +1,28 @@
+export default {
+	abc: [
+		`X:1
+T:The Green Fields Of Rossbeigh
+R:reel
+L:1/16
+M:4/4
+N:Imported into *tuneTable* on 2026-05-15,
+N:from https://thesession.org/tunes/322#setting13094
+N:Setting entered in thesession by user “ceolachan” on 2008-10-18
+N:*abc-tools: convert to M:4/4 & L:1/16*
+N:(edited after importing)
+K:Edorian
+|:dc|B~E3 BAFB ABde fded|B~E3 BAFA BFAF EFGA|
+B2Bd BAFB ABde fded|B~E3 BAFA BFAF E2:|
+|:FA|B/c/def g2fe d~B3 dBAd|B/c/def g2fe dBAF E2ef|
+~g3a gfeg ~f3g fedB|ABde fgfe dBAF E2:|`
+	],
+	parts: "AABB",
+	theSessionId: 322,
+	theSessionSettingId: 13094,
+	references: [
+		{
+			notes:
+				"One of the first reels I ever learnt; and it was from a (rather well written and detailed) score."
+		}
+	]
+};
