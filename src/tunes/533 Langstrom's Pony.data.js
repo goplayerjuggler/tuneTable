7643ede06c616063b2f824311ce565e01de6123d
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-18 02:20:06 +0200

    langrstrom's pnoy

---
+++ src/tunes/533 Langstrom's Pony.data.js
@@ -0,0 +1,32 @@
+export default {
+	aka: "The Langstern Pony",
+	abc: `X:1
+T:Langstrom's Pony
+R:jig
+L:1/8
+M:12/8
+N:Imported into *tuneTable* on 2026-05-18,
+N:from https://thesession.org/tunes/61#setting61
+N:Setting entered in thesession by user “Jeremy” on 2001-05-25
+N:*abc-tools: convert to M:12/8*
+K:Amixolydian
+|:fed cAA EAA cAA|fed cAA BGB dcB|
+fed cAA EAA cAA|faf gfe dBG Bcd:|
+|:cee dff cee ecA|cee g2e dBG Bcd|
+cee dff cee efg|faf gfe dBG Bcd:|
+|:Ace a2f ecA ecA|GBd g2e dBG Bcd|
+eaf g2e fed ecA|a2f gfe dBG Bcd:|
+|:AEA A2d cAc ecA|AEA AB=c BGB d^cB|
+AEA A2d cAc efg|faf gfe dBG Bcd:|`,
+	theSessionId: 61,
+	theSessionSettingId: 61,
+	parts: "AABBCCDD",
+	references: [
+		{
+			album: "Waiting For A Call",
+			artists: "Tommy Peoples, fiddle",
+			notes: "Paidin O’Rafferty / The Langstern Pony",
+			url: "https://open.spotify.com/track/5b527aij0Mu06lww1dzdm2"
+		}
+	]
+};
