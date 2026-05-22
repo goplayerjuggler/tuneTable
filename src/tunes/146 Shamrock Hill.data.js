bd4f4f06f870b1695f34db867d1ae0f113aacf6d
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-22 10:28:06 +0200

    tunes
    double bar length

--- src/tunes/146 Shamrock Hill.data.js
+++ src/tunes/146 Shamrock Hill.data.js
@@ -3,15 +3,16 @@ export default {
 T:Shamrock Hill
 C:Sean Ryan
 R:reel
-L:1/8
+L:1/16
 M:4/4
 N:Imported into *tuneTable* on 2025-10-25,
 N:from https://thesession.org/tunes/2584#setting2584
 N:Setting entered in thesession by user “Conway” on 2004-02-29
+N:*abc-tools: convert to M:4/4 & L:1/16*
 K:Gmajor
-|:DGBG dGBG|~c3 d efga|bg ~g2 agef|gedB AGEG|
- DGBG dGBG|~c3 d efga|bg ~g2 aged|1 cAFA ~G3 E:|2 cAFA G2 ga||
- |:bg ~g2 agef|gedB AGEG|DB, ~B,2 DEGA|Bdgb ageg|
- bg ~g2 agef|gedB AGEG|DB, ~B,2 DEGA|1 BdAF G2 ga:|2 BdAF ~G3 E||`,
+|:DGBG dGBG ~c3d efga|bg~g2 agef gedB AGEG|
+DGBG dGBG ~c3d efga|bg~g2 aged[1 cAFA ~G3E:|2 cAFA G2ga||
+|:bg~g2 agef gedB AGEG|DB,~B,2 DEGA Bdgb ageg|
+bg~g2 agef gedB AGEG|DB,~B,2 DEGA[1 BdAF G2ga:|2 BdAF ~G3E||`,
 	theSessionId: 2584
 };
