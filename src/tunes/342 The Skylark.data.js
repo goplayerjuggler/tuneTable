8fa3d7b6d19b03aa97a00404ac66fc394dc965c0
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-26 01:32:41 +0200

    tunes
    fix syntax error

--- src/tunes/342 The Skylark.data.js
+++ src/tunes/342 The Skylark.data.js
@@ -27,7 +27,7 @@ N:from https://thesession.org/tunes/632#setting35314
 N:Setting entered in thesession by user “JACKB” on 2019-07-16
 N:*abc-tools: convert to M:4/4 & L:1/16*
 N:(todo: not yet revised)
-KDmajor
+K:Dmajor
 |:agfg efdB AF F2 DFAd|BG G2 (3EFG E2 FA A2 FAdf|
 agfg efdB AF F2 DFAd|BG G2 E2 ag f2 eg fd d2:|
 |:a2 fd Adfd e3d cA A2|a2 fa bged Beed efge|
