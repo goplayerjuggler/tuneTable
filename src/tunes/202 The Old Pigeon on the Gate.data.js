47c335c5f61df004e0ebb311a6f7aebad29c6305
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-25 17:18:28 +0200

    tunes - The Old Pigeon On The Gate

--- src/tunes/202 The Old Pigeon on the Gate.data.js
+++ src/tunes/202 The Old Pigeon on the Gate.data.js
@@ -13,25 +13,18 @@ K:Gmixolydian
 |:dG~G2 GedG G2ce dcAG|FEFd cF~F2 EFeg fdcA|
 dG~G2 GedG G2ce dcAG|F2eg abga fdcA G2~c2:|
 |:dg~g2 fagf dg~g2 bgag|dc~c2 Bcde f2ga fdcA|
-G2~g2 gagf dg~g2 bgag|dc~c2 gceg fdcA G2~c2:|`,
-		`
-X:2
-T:The Pigeon on the Gate
-F:https://donegalfiddlemusic.bandcamp.com/album/a-feeling-in-the-blood
-S:Néillidh Boyle
-D:A Feeling In The Blood
-N:Néillidh Boyle's version
-N:1. There are two different N. Boyle recordings on this album
-N:2. Compare with more commonly played tunes: a) (The Pigeon On The Gate)[ttId=201]
-b) The Old Pigeon On The Gate (see the link to thesession).
-N:(just an incomplete scrap)
-R:reel
-M:4/4
-L:1/16
-K:Gmix
-dG~G2 GedG G2ce dcAG | FEFd cF~F2 f2ga fdcA |`
+G2~g2 gagf dg~g2 bgag|dc~c2 gceg fdcA G2~c2:|`
 	],
-
+	references: [
+		{
+			url: "https://donegalfiddlemusic.bandcamp.com/album/a-feeling-in-the-blood",
+			album: "A Feeling In The Blood",
+			artists: "Néillidh Boyle, fiddle",
+			notes: `1. There are two different N. Boyle recordings of this here, under the name “The Pigeon on the Gate” – no “Old”.
+2. Compare with a more commonly played reel, The Pigeon On The Gate)[ttId=201]
+3. [on thesession](theSessionId=1596) there are some other settings it could be worth looking into – a note to myself…`
+		}
+	],
 	theSessionId: 1596,
 	theSessionSettingId: 24503,
 	parts: "AABB",
