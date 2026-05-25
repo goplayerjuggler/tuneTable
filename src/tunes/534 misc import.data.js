e578510c8d48b52047dcb62fe823d2a141149ae3
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-26 00:59:08 +0200

    tunes
    waverley

--- src/tunes/534 misc import.data.js
+++ src/tunes/534 misc import.data.js
@@ -164,54 +164,6 @@ gfg edB dgb age|d^cd gdB AGF G2:|`
 		theSessionSettingId: 13367
 	},
 	{
-		parts: "AABB",
-		abc: `X:1
-T:Waverly
-C:Art Galbraith
-O:USA;Missouri.
-S:John Carty, banjo; Michael McGoldrick, uillean pipes
-H:https://racketrecords.bandcamp.com/track/waverly-r-l-m-irt-n-sheamuis
-A:At Our Leisure
-R:reel
-L:1/16
-M:4/4
-N:Imported into *tuneTable* on 2026-05-18,
-N:from https://thesession.org/tunes/22069#setting44877
-N:Setting entered in thesession by user “dersulle” on 2022-09-23
-N:*abc-tools: convert to M:4/4 & L:1/16*
-N:(edited after importing)
-N:---
-N:Played in the album in a set: Waverly / Ríl Máirtín Sheamuis.
-N:They (Carty & McGoldrick) also play this set on [this video](https://www.facebook.com/watch/?v=1209416829844418), 
-N:with Michael McCague and Matt Griffin (on bouzouki & guitar - not 
-N:sure who plays which!)
-N:McGoldrick calls this an old-timey tune; he 
-N:got it from the fiddler Tatiana Hargreaves.
-K:Dmajor
-|:D2E2 G2AG BGAG BddB|d2de B2A2 GBAG EGGE|
-D2E2 G2AG BGAG BddB|efed B2BA[1 GBAG EGGE:|2 GBAG EGGf||
-|:[M:3/4]g2gf gfed B2d2|[M:4/4]efed B2BA GBAG EGGf|
-[M:3/4]g2gf gfed B2d2|[M:4/4]efed B2BA[1 GBAG EGGf:|2 GBAG EGGE||`,
-		theSessionId: 22069,
-		theSessionSettingId: 44877,
-		tags: "crooked",
-		references: [
-			{
-				artists: "Tatiana Hargreaves, fiddle; Bruce C. Molsky, guitar",
-				url: "https://www.youtube.com/watch?v=vvpAFdNj4vk",
-				album: "Started Out To Ramble (2009)",
-				notes: `The [whole album](https://youtube.com/playlist?list=OLAK5uy_mMTFfLFYXA7sDQRDhX536Vt1sGXxPFg0I) is well worth a listen.`
-			},
-			{
-				artists: "Tricia Spencer, fiddle; Howard Rains, guitar",
-				url: "https://www.youtube.com/watch?v=0j4oYP_NZa4",
-				notes: `Another original USA old-timey setting.
-Played AAB (and not AABB as in the other settings given here).`
-			}
-		],
-		fileDate: "2026-05-21"
-	},
-	{
 		fileDate: "2026-05-18",
 		abc: `X:1
 T:O'Connor's
