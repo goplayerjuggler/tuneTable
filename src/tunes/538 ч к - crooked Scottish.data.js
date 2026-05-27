774604300de68de910cdf00747c9cb632e7fa80f
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-05-27 11:22:16 +0200

    tunes
    червона калина as a crooked Scottish - tweaks

--- src/tunes/538 ч к - crooked Scottish.data.js
+++ src/tunes/538 ч к - crooked Scottish.data.js
@@ -6,18 +6,19 @@ export default {
 	ttId: 538,
 	abc: `
 X:1
-T:червона калина as a crooked Scottiche
+T:червона калина as a crooked Scottish
 C:Malcolm Schonfield
-H:(2026-05-19) This setting of [Ой, у лузі червона калина](ttId=514) came to me today. Rhythmically I think it's related to the Scottiche (a French folk dance). 
-H:(2026-05-20) I revised the score. It's still a little vague in my mind; there are several directions it could go in. But maybe these notes could be helpful.
-R:Scottiche
+H:(2026-05-19) This setting of [Ой, у лузі червона калина](ttId=514) came to me today. Rhythmically I think it's related to the Scottish (a French folk dance). 
+H:(2026-05-20) I revised the score. It's still a little vague in my mind; there are several directions it could go in. But maybe these notes could be helpful. 
+H:(2026-05-27) further small revisions
+R:Scottish
 M:4/4
 L:1/16
 K:Dm
 D2A,D .F2!>!E2- EFED ^CA,=B,C | D^CDE FAEF DdAF DA,=B,C | D2A,D .G2!>!F2- FED^C D2A,2 | [M:3/4]D3E FGEF DCDE  ||
 |: [M:5/4]F2FA .c2!>!c2- cdcB AGAB AGFE | [M:4/4] DCDE FGA2- ABAG FEDF | [M:5/8] [1 A2GA FE D"<("C">)"DE :| [2 [M:3/8]A2G2FE | [M:2/4] D4 z4 |]
 P:variation on the A
-D2A,D .F2!>!E2- EFED ^CA,=B,C |"^v1"y [M:3/4] DCDE FAEF D2z2 | [M:4/4]D2A,D .G2!>!F2- FED^C D2A,2 | [M:3/4]D"<("">)"CDE "^v2"FAEF D2 (3CDE ||
+D2A,D .F2!>!E2- EAE^C A,=B,CA, |"^v1"y [M:3/4] DCDE FAEF D2z2 | [M:4/4]D2A,D .G2!>!F2- FED^C D2A,2 | [M:3/4]D"<("">)"CDE "^v2"FAEF D2 (3CDE ||
 P:variation on the B
 |: [M:5/4]F2FA .c2!>!c2- cdcB AGAB AdAG | [M:4/4] FEDE FGA2- ABAG FEDF | [1 [M:3/4] AdAG FDEC D3E  :| [2 [M:3/8]A2G2FE | [M:2/4] D4 z4 |]
 P:more variations
