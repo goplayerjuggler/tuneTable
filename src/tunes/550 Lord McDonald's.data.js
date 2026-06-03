9d2445ccac7ec4b72c2701893e9aa41f39d1ac08
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-06-03 10:06:43 +0200

    tunes

--- src/tunes/550 Lord McDonald's.data.js
+++ src/tunes/550 Lord McDonald's.data.js
@@ -5,16 +5,6 @@ T:Lord McDonald's
 R:reel
 L:1/16
 M:4/4
-N:Based on Liam O'Flynn's setting:
-N:X: 1 T: Lord McDonald M: 4/4 L: 1/8 R: reel K: Gmaj ~G3B DGBG|cGBG AGEG|DGBG dG
-N:(3Bcd|gedc BGGF| ~G3B dG~G2|cGBG AGEG|BGAF GEDB|DEGA BG~G2|| BddB dBGB|defg
-N:aged|BddB dBGB|AB (3cBA BG~G2| BddB dBGB|defg aged|(3Bcd ef ~g3e|dBAc BG~G2||
-N:dgbg agbg|dgbg ageg|dgbg agbg|dedc BG~G2| dgbg agbg|dgbg ageg|bgaf gedB|dega
-N:bg~g2|| BddB dBGB|defg aged|BddB dBGB|AB (3cBA BG~G2| BddB dBGB|defg aged|(3Bcd
-N:ef ~g3e|dBAc BG~G2||
-N:I can hear Matt Molloy doing some other stuff. Bars 1 & 2 of the A-part are
-N:|~G3B DGAG|(3cBA (3BAG AGEG|. Bar 1of the B-part has C#s: |Bdd^c dBGB|.
-N:---
 N:Imported into *tuneTable* on 2026-06-03,
 N:from https://thesession.org/tunes/507#setting13432
 N:Setting entered in thesession by user “Dr. Dow” on 2006-03-13
