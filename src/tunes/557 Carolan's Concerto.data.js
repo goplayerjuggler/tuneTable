fbdf302ad3ed48eb7d743eff6a2b1514c1eb3c62
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-06-10 00:08:30 +0200

    Carolan's Concerto

---
+++ src/tunes/557 Carolan's Concerto.data.js
@@ -0,0 +1,25 @@
+export default {
+	abc: `X:1
+T:Carolan's Concerto
+C:Turlough O'Carolan
+R:reel
+L:1/8
+M:4/2
+N:Imported into *tuneTable* on 2026-06-09,
+N:from https://thesession.org/tunes/788#setting13923
+N:Setting entered in thesession by user “Kevo32A” on 2007-08-10
+N:*abc-tools: convert to M:4/2 & L:1/8*
+N:Unlike most reels, here I prefer longer bars with quavers
+N:rather than semiquavers.
+K:Dmajor
+(3ABc|:d2 dd d2 cB ABGA F2A2|E2A2 D2A2 Bcde dcBA|
+d2a2 fgaf efge fgaf|gabg fdfa gfed dcBA|
+BdBd gdgd AdAd fAfA|GBGB efed c2Bc A3G|
+FGAF EFGE FGAF GABG|A2d2 fedc d6:|
+(3ABc|:d2b2 agfe d2a2 c2a2|B2gB ABcd ecBA d2df|
+edcB caaa Bggg Afff|edcB caaa Bggg bgbg|
+afaf edcB cABG AFGE|DAFA DAFA GEBE GEBE|
+FAdc BAGF EFGA Bcde|A2d2 fedc d6:|`,
+	theSessionId: 788,
+	theSessionSettingId: 13923
+};
