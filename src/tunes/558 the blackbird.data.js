81d32546a60c1a39051b716633cf008b9a545bc5
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-06-11 00:37:16 +0200

    tunes

---
+++ src/tunes/558 the blackbird.data.js
@@ -0,0 +1,59 @@
+export default {
+	parts: "AABB",
+	abc: [
+		`X: 1
+T: The Blackbird
+S:Michael Coleman, fiddle; ?, piano
+R: hornpipe
+F:https://youtu.be/JkejkLskcUg
+F:http://www.juneberry78s.com/sounds/mo33001t03.mp3
+M: 4/2
+N:An attempt to capture Michael Coleman’s setting.
+N:1. I started this in order to understand what was happening in the B part, but 
+N: I got distracted by the details of the ornaments & phrasing, and decided to
+N: have a go at sketching them out too. This is just a transcription of the first
+N: time round; on the recording it gets played four times and each time 
+N: is a bit different. 
+N:2. I believe this is really a set dance, not a hornpipe; but I’ve put “hornpipe”
+N:so as to see it amongst the hornpipes as it shares a lot with them rhythmically.
+L: 1/8
+K: D mixo
+G>A|BB3 ((3c"<("">)"dc) ((3B"<("">)"cB) {B}A2^G>B A>Bc>A|d>cd>e d>cA>F G>FG>F DD-D>E|
+F>EF>G A>df>e d>d-{e}d>d- {e}d>cA>F|[M:3/2]G>EF>E (3DED ^C>E D2 G>A|
+(3Bcd B>d c>dB>c (3ABA ^G>B A>Bc>B | A>df>e d>^cA>F G>FG>F DD-D>E|
+F>EF>G A>df>e d>d- d>d- d>cA>F|[M:3/2]G>EF>E (3DED ^C>E D2
+M:4/2
+K:D
+|:d>e|f>dd>e .f2g2 [L:1/16](a3(3b/a/g/ [L:1/8]a)>g f>ga>f | g2{a}g>f g>ba>f g>ag>f dd-d>e |
+      (f<d)d>e [d2f2]g2 a2- a>g f>ga>f    | [M:5/2]g>ag>f dd-d>c d>ef>g a2 a>g f>ga>f | 
+M:3/2
+g>f (3efg f2e>f d>cA2 | A>Bc>d e>f (3gfe a2 a2|
+M:4/2
+[(A2F2]A)>G F>GA>B c>d e2 d>cA>F | [M:3/2] G>EF>E (3DED C>E D2 :| 
+`,
+		`X: 2
+T: The Blackbird
+N: A slightly simplified setting.
+N:The same structure as the previous entry, but without all the
+N:ornaments; I may have tweaked a few passing notes too.
+S:Michael Coleman
+R: hornpipe
+M: 4/2
+L: 1/8
+K: D mixo
+G>A|B4 c2 B2 A2^G>B A>Bc>A|d>cd>e d>cA>F G>FG>F D2 D>E|
+F>EF>G A>df>e d2d>e d>cA>F|[M:3/2]G>EF>E (3DED ^C>E D2 G>A |
+(3Bcd B>d c>dB>c A>B^G>B A>Bc>B | A>df>e d>cA>F G>EG>F D2D>E|
+F>EF>G A2f>e d>^cd>e d>cA>F|[M:3/2]G>EF>E D2 ^C>E D2 
+M:4/2
+K:D
+|:d>e|f>dd>e f2g2 a2a>g f>ga>f  | g2g>f g>ba>f g>ag>f d2d>e |
+      f>dd>e f2g2 a2-a>g f>ga>f | [M:5/2]g>ag>f d2d>c d>ef>g a2a>g f>ga>f | 
+M:3/2
+g>f (3efg f2e>f d>cA2 | A>Bc>d e>f (3gfe a2 a2|
+M:4/2
+A2-A>G F>GA>B c>d e2 d>cA>F | [M:3/2] G>EF>E (3DED C>E D2 :| 
+`
+	],
+	tags: "crooked"
+};
