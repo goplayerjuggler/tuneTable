f96fa137aad7dd59e6b4adbdab4ccd02af6adb3d
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-07-01 10:52:19 +0200

    rename

---
+++ src/tunes/561 Danse du barbier.data.js
@@ -0,0 +1,19 @@
+export default {
+	parts: "AABB",
+	abc: [
+		`
+X:1
+T:Danse du barbier
+R:three-two
+S: Laurie Hart, fiddle
+D: Danse ce soir! traditional tunes of Québec 
+F: https://lauriehart.bandcamp.com/track/danse-du-barbier-cotillon-de-baie-ste-catherine-la-marmotteuse
+Z: Malcolm Schonfield %2024-01- (08-09)
+L:1/8
+M:3/2
+K:G
+[P:A]B.d .d.e dc/B/| A.d .d.e d>c | Bd de dc/B/ | Ac BA [1 G>A:| [2 G>G ||
+[P:B] |:DG FG A/B/A/G/| Ad cB/c/ A>A | DG FG A/B/A/G/ | Ad AB [1 G>G :| [2 G>!D.C.!A |] 
+`
+	]
+};
