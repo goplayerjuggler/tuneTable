eaa21175cc55aa433ef790f7649450218b9c2b58
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-07-15 20:26:32 +0200

    temple hill
    bare bones version

--- src/tunes/561 temple hill.data.js
+++ src/tunes/561 temple hill.data.js
@@ -1,46 +1,48 @@
 export default {
+	aka: "Cnocán An Teampaill",
 	tags: "crooked",
 	groups: "alora",
 	parts: "AB",
-	structure: "4.4.3;2.(3.3 + 4.3)",
+	structure: "2.2.4.3;2.(3.3 + 4.3)",
 	abc: [
 		`
 X:1
 T:Temple Hill
 R:jig
 M:12/8
-S:Paddy O'Brien, accordion
-H: (2026-07-07) This score is a simple adaptation of Bickford’s score;  I've moved bar lines around to show how I see the structure. (And I added a “segno” and a “D.S.”.)
-H: [Mark Bickford’s score](https://www.itma.ie/scores/pob1268_itma0290338_016/) is is on the excellent, new itma score database, and it synchs very nicely with the audio recording by Paddy O’Brien.
-H: The way I see it, and the way I've put it here, the A part is a standard jig except without a repeat, and the B part is a sort of a crooked slip jig. Written in 6/8, it makes seven bars, whereas normal jigs have eight bars of 6/8. 
-H: I think it's a fine tune. Very interesting too; I'm only aware of a handful of such crooked jigs. Currently in this tune list there are only three other crooked jigs.
-H: I discovered this jig very recently via the ITMA page, [Mark Bickford’s picks from the Paddy O’Brien Tune Collection](https://www.itma.ie/playlists/mark-bickford-tune-selection/?track=10).
-H:
-H: There's some tantalising info alongside the score: 
-H: 
-H: ‘Temple Hill’ is the final tune of a long descriptive set of tunes similar to ‘The Fox Chase’, but in this case it sets the mood of the Battle of Cnoc na nDos (also known as Knockinoss), fought in 1647, that ended with the death of Alastair ‘Colkitto’ MacDonald, a Scottish general who fought with the Irish side. 
-H: 
-H: I presume this is a note written by Paddy O'Brien. I wonder what other info is available about this set of tunes.
+H:This is my attempt (as of 2026-07-15) to give the bare bones of the Paddy O’Brien setting, with some chords that are just meant to be a starting point. 
+H:The next setting is my tweaked version of Bickford’s transcription, with links to a recording by Paddy O’Brien.
 L:1/8
 K:A min
-c | !segno! cAG A/B/AA ABA A2 d | cAG A/B/AA  ABA GAB | 
-    cAG A/B/AA ABA A2{/^c} d | cAG A/B/AA   ABA GED || 
-    [M:9/8] EDE ccE ccE | [M:12/8] EDE cde dcA  GED |
-    [M:9/8] EDE ccE ccE | [M:12/8] EFE cde dcA GAB !D.S.! |]  
+"Am" cAG A2A ABA A2d | "Am" cAG A2A ABA "G" [1 GAB :| [2 GED ||
+[M:9/8] "C" EDE ccE ccE | [M:12/8] "C"EDE cde dcA "G"GED |
+[M:9/8] "C" EDE ccE ccE | [M:12/8] "C(Dm)"EFE cde dcA "G"GAB !D.C.! |]  
 `,
 		`
 X:1
 T:Temple Hill
 R:jig
 M:12/8
-N: Same as the previous score, but with some chords I added. Just a sketch.
+S:Paddy O'Brien, accordion
+H: (2026-07-07) This score is a simple adaptation of Bickford’s score;  I’ve moved bar lines around to show how I see the structure. (And I added a “segno” and a “D.S.”.)
+H: [Mark Bickford’s score](https://www.itma.ie/scores/pob1268_itma0290338_016/) is is on the excellent, new itma score database, and it synchs very nicely with the audio recording by Paddy O’Brien.
+H: The way I see it, and the way I’ve put it here, the A part is a standard jig except without a repeat, and the B part is a sort of a crooked slip jig. Written in 6/8, it makes seven bars, whereas normal jigs have eight bars of 6/8. 
+H: I think it’s a fine tune. Very interesting too; I’m only aware of a handful of such crooked jigs. Currently in this tune list there are only three other crooked jigs.
+H: I discovered this jig very recently via the ITMA page, [Mark Bickford’s picks from the Paddy O’Brien Tune Collection](https://www.itma.ie/playlists/mark-bickford-tune-selection/?track=10).
+H:
+H: There’s some tantalising info alongside the score: 
+H: 
+H: ‘Temple Hill’ is the final tune of a long descriptive set of tunes similar to ‘The Fox Chase’, but in this case it sets the mood of the Battle of Cnoc na nDos (also known as Knockinoss), fought in 1647, that ended with the death of Alastair ‘Colkitto’ MacDonald, a Scottish general who fought with the Irish side. 
+H: 
+H: I presume this is a note written by Paddy O’Brien. I wonder what other info is available about this set of tunes.
 L:1/8
 K:A min
-c | !segno! "Am" cAG A/B/AA ABA A2 d | "Am" cAG A/B/AA  ABA "G" GAB | 
-    "Am"cAG A/B/AA ABA A2{/^c} d | cAG A/B/AA   ABA "G"GED || 
-    [M:9/8] "C"EDE ccE ccE | [M:12/8] "C"EDE cde dcA  "G"GED |
-    [M:9/8] "C"EDE ccE ccE | [M:12/8] "C"EFE cde dcA "G"GAB !D.S.! |]  
+c | !segno! cAG A/B/AA ABA A2 d | cAG A/B/AA  ABA GAB | 
+    cAG A/B/AA ABA A2{/^c} d | cAG A/B/AA   ABA GED || 
+    [M:9/8] EDE ccE ccE | [M:12/8] EDE cde dcA  GED |
+    [M:9/8] EDE ccE ccE | [M:12/8] EFE cde dcA GAB !D.S.! |]  
 `
 	],
-	ttId: 561
+	ttId: 561,
+	theSessionId: 6567
 };
