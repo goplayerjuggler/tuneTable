6ffa4b974bff4b949c20a2c5ce128d8d4043c18e
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-07-15 02:08:05 +0200

    harvest home

--- src/tunes/25 The Harvest Home.data.js
+++ src/tunes/25 The Harvest Home.data.js
@@ -3,14 +3,30 @@ export default {
 T:The Harvest Home
 R:hornpipe
 L:1/8
-M:4/4
+M:4/2
 N:Imported into *tuneTable* on 2025-10-25,
 N:from https://thesession.org/tunes/49#setting49
 N:Setting entered in thesession by user “Jeremy” on 2001-05-25
+N:*abc-tools: convert to M:4/2*
 K:Dmajor
-|:AF|DAFA DAFA|defe dcBA|eAfA gAfA|(3efe (3dcB AGFE|
- DAFA DAFA|defe dcBA|eAfA gfec|d2 f2 d2:|
- |:cd|eAAA fAAA|gAfA eAAA|eAfA gAfA|(3efe (3dcB (3ABA (3GFE|
- DAFA DAFA|defe dcBA|eAfA gfec|d2 f2 d2:|`,
+|:AF|DAFA DAFA defe dcBA|eAfA gAfA (3efe (3dcB AGFE|
+DAFA DAFA defe dcBA|eAfA gfec d2 f2 d2:|
+|:cd|eAAA fAAA gAfA eAAA|eAfA gAfA (3efe (3dcB (3ABA (3GFE|
+DAFA DAFA defe dcBA|eAfA gfec d2 f2 d2:|`,
+	references: [
+		{
+			url: "https://donegalfiddlemusic.bandcamp.com/track/the-harvest-home-the-green-mountain-hornpipes",
+			album: "A Feeling In the Blood (1930s)",
+			notes: `The Harvest Home / The Green Mountain (reel)
+	A Néillidh Boyle recording that made a big impression on me.`,
+			artists: "Néillidh Boyle, fiddle"
+		},
+		{
+			url: "https://donegalfiddlemusic.bandcamp.com/track/harvest-home",
+			album: "A Feeling In the Blood (1930s)",
+			notes: `Another Néillidh Boyle recording that’s well worth checking out, with some glissando effects that only he does AFAIK. It’s preceded by a [recording of him talking about the tune](https://donegalfiddlemusic.bandcamp.com/track/harvest-home-speech). One interesting thing (among others) is he calls it a reel – he doesn’t call it a hornpipe.`,
+			artists: "Néillidh Boyle, fiddle"
+		}
+	], //
 	theSessionId: 49
 };
