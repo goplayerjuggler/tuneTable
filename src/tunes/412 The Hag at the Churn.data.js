951bd9a30a3145aaffbb0ef3711b8df4f6d48442
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-07-16 11:20:06 +0200

    tunes
    fix rendering issue!

--- src/tunes/412 The Hag at the Churn.data.js
+++ src/tunes/412 The Hag at the Churn.data.js
@@ -3,7 +3,8 @@ export default {
 	incipit: `
 X:1
 T:The Hag at the Churn
-R:single jigH. Norbeck’s site gives some interesting background on this tune.
+R:single jig
+N:H. Norbeck’s site gives some interesting background on this tune.
 M:6/8
 K:Dmix
 A2G ADD A2G Adc|A2G ADD EFG EFG:|`,
