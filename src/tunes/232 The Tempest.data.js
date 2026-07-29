881c563296cce6f75149ab07531d095ca683ffcd
goplayerjuggler <malcolm_schonfield@yahoo.fr>
2026-07-29 10:11:55 +0200

    tunes

--- src/tunes/232 The Tempest.data.js
+++ src/tunes/232 The Tempest.data.js
@@ -2,16 +2,17 @@ export default {
 	abc: `X:1
 T:The Tempest
 R:reel
-L:1/8
+L:1/16
 M:4/4
 N:Imported into *tuneTable* on 2025-10-27,
 N:from https://thesession.org/tunes/1004#setting14222
 N:Setting entered in thesession by user “birlibirdie” on 2012-07-19
+N:*abc-tools: convert to M:4/4 & L:1/16*
 K:Edorian
-|:e2dB AFF2|EFdF eFdF|e2dB AF F2|EFBF AFEF|
- e2dB AFF2|EFdF eFdF|EFGB AFdF|FEdA FEE2:|
- e2fe dBAB|Beed BAFA|e2fe defg|fedf e3B|
- fgeg fedB|edBA BAFA|EFGB AFdF|FEdA FEE2:|`,
+|:e2dB AFF2 EFdF eFdF|e2dBAF F2 EFBF AFEF|
+e2dB AFF2 EFdF eFdF|EFGB AFdF FEdA FEE2:|
+e2fe dBAB Beed BAFA|e2fe defg fedf e3B|
+fgeg fedB edBA BAFA|EFGB AFdF FEdA FEE2:|`,
 	references: [
 		{
 			artists: "Denis Ryan, fiddle",
@@ -19,5 +20,6 @@ K:Edorian
 		}
 	],
 	theSessionId: 1004,
-	theSessionSettingId: 14222
+	theSessionSettingId: 14222,
+	parts: "AABB"
 };
