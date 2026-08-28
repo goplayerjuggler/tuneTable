423748b511387428f7d87cf84efbdf5a8e5ecfe2
Malcolm S <malcolm_schonfield@yahoo.fr>
2026-08-28 10:34:45 +0200

    tunes

--- src/tunes/508 Valse à Chabrier.data.js
+++ src/tunes/508 Valse à Chabrier.data.js
@@ -1,5 +1,6 @@
 export default {
-	abc: `X:1
+	abc: `
+X:1
 T:Valse à Chabrier
 R:Waltz
 O:France;Auvergne;Artense.
@@ -7,23 +8,19 @@ S:Clémence Cognet, violon ; Noëllie Nioulou, violoncelle
 D:Les poufs à cordes / La Trotteuse (2013)
 Z:abc-transcription Malcolm Schonfield
 Z:abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
-N:1. De toute évidence, cette valse magnifique nous vient d’[Antonin Chabrier](http://www.violoneux.fr/wiki/Antonin_Chabrier),
-N:violonneux de l’Artense.
-N:2. Les accords que j’ai rajoutés ne sont qu’une esquisse harmonique. 
-N:Au lieu de C7 on pourrait très bien mettre autre chose, 
-N:genre des accords ouverts; ou rien !
-N:3. J’ai donné quelques indications sommaires sur les phrasés, mais
-N:il vaut mieux écouter ça si on peut !
+H:1. De toute évidence, cette valse magnifique nous vient d’[Antonin Chabrier](http://www.violoneux.fr/wiki/Antonin_Chabrier), violonneux de l’Artense.
+H:2. Les accords que j’ai rajoutés ne sont qu’une esquisse harmonique. J'ai mis des “Bb (Gm)” mais ça pourrait très bien être des C7 ou autre chose, genre des accords ouverts; ou rien ! 
+H:3. J’ai donné quelques indications sommaires sur le phrasé, mais il vaut mieux écouter ça si on peut !
 H: Il y a quelque chose de magique ici…
 M:3/4
 L:1/4
 K:C mixo
-"C"G~AG|cB~A|Gcd|e-!>!~e/d/e/f/|g.ag|"C7"fd/e/d/e/|fg/f/e/d/|"C"e>dc/A/|
-GAG|cB~A|Gcd|e-!>!~e/d/e/f/|g.ag|"C7"fd/e/d/e/|fg/f/e/d/|[1c3:|[2c2.G||
-"C"c>Bc/d/|edc|e>de/f/|g3|Ggf|e/f/g/f/e/f/|"C7"de/f/e/d/|"C"e/d/c/G/e/d/|
-c>Bc/d/|edc|e>de/f/|g3|G~g~f|e/f/g/f/e/f/|"C7"de/f/e/d/|c3||
-Gga|g3|e/f/e/d/c/d/|e3|e/f/e/d/e/f/|g.fe|"C7"de/f/e/d/|"C"e>dc/A/|
-Gga|g3|e/f/e/d/c/d/|e3|e/f/e/d/e/f/|g.fe|"C7"de/f/e/d/|"C"c3|]
+"C"G~AG|cB~A|Gcd|e-!>!~e/d/e/f/|g.ag|"Bb (Gm)"fd/e/d/e/|fg/f/e/d/|"C"e>dc/A/|
+GAG|cB~A|Gcd|e-!>!~e/d/e/f/|g.ag|"Bb (Gm)"fd/e/d/e/|fg/f/e/d/|[1c3:|[2c2.G||
+"C"c>Bc/d/|edc|e>de/f/|g3|Ggf|e/f/g/f/e/f/|"Bb (Gm)"de/f/e/d/|"C"e/d/c/G/e/d/|
+"C"c>Bc/d/|edc|e>de/f/|g3|G~g~f|e/f/g/f/e/f/|"Bb (Gm)"de/f/e/d/|"C"c3||
+"C"Gga|g3|e/f/e/d/c/d/|e3|e/f/e/d/e/f/|g.fe|"Bb (Gm)"de/f/e/d/|"C"e>dc/A/|
+"C"Gga|g3|e/f/e/d/c/d/|e3|e/f/e/d/e/f/|g.fe|"Bb (Gm)"de/f/e/d/|"C"c3|]
 `,
 	parts: "AABC",
 	groups: "alora",
