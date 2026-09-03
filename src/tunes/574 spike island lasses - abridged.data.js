34e4603a641715f010b2a25cc622fb98b99e3988
Malcolm S <malcolm_schonfield@yahoo.fr>
2026-09-03 15:32:04 +0200

    tunes

--- src/tunes/574 spike island lasses - abridged.data.js
+++ src/tunes/574 spike island lasses - abridged.data.js
@@ -8,7 +8,7 @@ S:Doireann Glackin, fiddle; Sarah Flynn, concertina
 F:https://thehousekeepers.bandcamp.com/track/spike-island-lasses-pride-of-rathmore-reels
 N:Track 1: Spike Island Lasses / Pride of Rathmore (Reels) 
 R:reel
-H:This is clearly a close relative of the (longer) [reel by the same name](theSessionId=737). It's a shortened/truncated/abridged version, one might say. But it's a very worthy setting; just as good as the other, more commonly-heard setting. They're both quite different. Enough so for me to give it a separate entry.
+H:This is clearly a close relative of the [(longer) reel by the same name](theSessionId=737). It's a shortened/truncated/abridged version, one might say. But it's a very worthy setting; just as good as the other, more commonly-heard one. Enough so for me to give it a separate entry, anyway.
 H:In my mind I hear this one in D, but after checking, it seems Glackin and Flynn recorded it in the unusual key of A flat!
 Z:abc-transcription Malcolm Schonfield
 Z:abc-copyright CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
@@ -19,5 +19,6 @@ F|!segno!GEDB, EA,A,B, (3CB,A, DB, CA,A,F| GEDB, EAAB (3cBA BG AcBA |
 GEDB, EA,A,B, (3CB,A, DB, CA,A,F| GEDB, EAAB (3cBA BG A3B ||
 c2Ac B2GB AcBA GEE2 | c2Ac BAGA Beed BAA2| 
 e3c d2cB AcBA GEDF | EAAG A3B (3cBA BG AcBA !D.S.! |]`,
-	ttId: 574
+	ttId: 574,
+	contourShift: 1
 };
