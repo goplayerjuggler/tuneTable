7245c7f3f8e7861e767c8fe479079cdbcd558549
Malcolm S <malcolm_schonfield@yahoo.fr>
2026-09-09 01:39:24 +0200

    tunes

--- src/tunes/176 Brenda Stubbert's.data.js
+++ src/tunes/176 Brenda Stubbert's.data.js
@@ -3,16 +3,18 @@ export default {
 T:Brenda Stubbert's
 C:Jerry Holland
 R:reel
-L:1/8
+L:1/16
 M:4/4
 N:Imported into *tuneTable* on 2025-10-25,
 N:from https://thesession.org/tunes/727#setting727
 N:Setting entered in thesession by user “seara” on 2002-06-03
+N:*abc-tools: convert to M:4/4 & L:1/16*
 K:Adorian
-|:B|A/A/A (BA) GAAB|A/A/A (BA) edde|G2 (BA) BGGB|c2 (BA) BGGB|
- A/A/A (BA) GAAB|A/A/A (BA) edda|gedB GABd|{d}e2 dB eAA:|
- |:B|A/A/A a2 A/A/A g2|Aage ageg|G2 (BA) BGGB|c2 (BA) BGGB|
- [1A/A/A a2 A/A/A g2|Aage agea|gedB GABd|{B}e2 dB eAA:|
- [2A/A/A (BA) GAAB|A/A/A (BA) edda|gedB GABd|{d}e2 dB eAA||`,
-	theSessionId: 727
+|:B|A/A/A(BA) GAAB A/A/ABA edde|G2(BA) BGGB c2BA BGGB|
+    A/A/A(BA) GAAB A/A/ABA edda|gedB GABd {d}e2dB eAA:|
+|:B|A/A/Aa2 A/A/ g2 Aage ageg|G2(BA) BGGB c2BA BGGB|
+  [1A/A/Aa2 A/A/Ag2 Aage agea|gedB GABd {B}e2dB eAA:|
+  [2A/A/A(BA) GAAB A/A/ABA edda|gedB GABd {d}e2dB eAA||`,
+	theSessionId: 727,
+	parts: "AABB"
 };
