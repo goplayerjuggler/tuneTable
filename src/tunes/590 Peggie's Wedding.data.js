b0b9c14e3353b7611d7551c70f89c5d027fc7825
Malcolm S <malcolm_schonfield@yahoo.fr>
2026-09-09 07:22:40 +0200

    Revise header and improve musical notation
    Updated the header comment for clarity and corrected some musical notation.

--- src/tunes/590 Peggie's Wedding.data.js
+++ src/tunes/590 Peggie's Wedding.data.js
@@ -7,16 +7,16 @@ D:At Our Leisure
 R: reel
 Z:abc-transcription Malcolm Schonfield
 N: A laid-back, slowish reel that's a close relative of [Over The Moor To Maggie](theSessionId=115)
-H:I jotted down the dots more or less from memory, after giving it a couple of listens.
+H:I've jotted down the dots more or less from memory. There are probably a few errors in there. 
 M: 4/2
 L: 1/8
 K: Gmaj
-|:BA|GFGA BABd efed BAGA|BAAB A2AG EAAc BAGD|
-     GFGA BABd efge BAGA|DGGA G2ED DGGA G2 :|
-|:ga|babg agaf gfge dBG2|eaab a2gaa eaab a2ga|
-babg agaf gfge dBG2|dgga g2ed egga g2:|gf
-|:efed Bdde gfge dBGB|EAAB A2GA EAAB A2BD|
-     efed Bdde gfge dBGB|DGGA G2ED EGGA [1G2Bd :| [2G2!!D.C.!|]
+|:BA|GFGA BABd efed BAGA|BAAB A2GA EAAc BAGD|
+     GFGA BABd efge dBAG|DGGA G2ED EGGA G2 :|
+|:ga|babg agaf gfge dBGB|eaab a2ga eaab a2ga|
+     babg agaf gfge dBG2|dgga g2ed egga g2:|gf
+|:efed Bdde gfge dBGB|EAAB A2GA EAAB A2Bd|
+     efed Bdde gfge dBAG|DGGA G2ED EGGA [1G2Bd :| [2G2!D.C.!|]
 `,
 		parts: "AABBCC",
 		ttId: 590
