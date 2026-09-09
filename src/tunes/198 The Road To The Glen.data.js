03a10d5822685624b9c583fcf25b7854f58c12a6
Malcolm S <malcolm_schonfield@yahoo.fr>
2026-09-09 08:01:13 +0200

    tunes

--- src/tunes/198 The Road To The Glen.data.js
+++ src/tunes/198 The Road To The Glen.data.js
@@ -3,15 +3,17 @@ export default {
 T:The Road To The Glen
 C:Ed Reavy
 R:reel
-L:1/8
+L:1/16
 M:4/4
 N:Imported into *tuneTable* on 2025-10-25,
-N:from https://thesession.org/tunes/2805#setting2805
+N:from 
+https://thesession.org/tunes/2805#setting2805
 N:Setting entered in thesession by user “Kerri Coombs” on 2004-04-12
+N:*abc-tools: convert to M:4/4 & L:1/16*
 K:Dminor
-|:FdcB AGFD|G2 dG eGdG|FEFG Acde|fedc A=BcA|
- d3 e f2 ag|fedc AFDE|1 F3 G AGFG|Addc d2 (3CDE:|2 F3 G AFDE|FDEC D2 de||
- f2 ag fedf|ecgc acgc|f2 ag fede|fdec Adde|
- f3 g (3agf (3gfe|fedc AFDE|F3 G AGFG|1 Addc d2 de:|2 Addc d2 (3CDE||`,
-	theSessionId: 2805
+|:FdcB AGFD G2dG eGdG|FEFG Acde fedc A=BcA|
+d3e f2ag fedc AFDE[1 F3G AGFG Addc d2 (3CDE:|2 F3G AFDE FDEC D2de||
+f2ag fedf ecgc acgc|f2ag fede fdec Adde|
+f3g (3agf (3gfe fedc AFDE|F3G AGFG [1 Addc d2de:|2 Addc d2 (3CDE||`,
+	theSessionId: 2805, parts:"AABB" 
 };
