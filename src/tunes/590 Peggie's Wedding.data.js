b89d882b59c1628d6ac37b8bb9b193a1f3fd1761
Malcolm S <malcolm_schonfield@yahoo.fr>
2026-09-14 10:51:34 +0200

    tunes

--- src/tunes/590 Peggie's Wedding.data.js
+++ src/tunes/590 Peggie's Wedding.data.js
@@ -1,25 +1,40 @@
 export default [
 	{
 		groups: "alora",
-		abc: `X: 1
+		abc: [
+			`X: 1
 T: Peggie's Wedding
 S:Michael McGoldrick, uileann pipes; John Carty, fiddle
 D:At Our Leisure
 R: reel
 Z:abc-transcription Malcolm Schonfield
 N: A laid-back, slowish reel that's a close relative of [Over The Moor To Maggie](theSessionId=115).
-H:I've jotted down the dots from memory, but haven't yet gotten round to checking. So there are probably a few errors here. 
+H:I've jotted down the dots from memory, but haven't yet gotten round to checking properly. So there may be a few errors here. 
 M: 4/2
 L: 1/8
 K: Gmaj
 |:BA|GFGA BABd efed BAGA|BAAB A2GA EAAc BAGD|
      GFGA BABd efed BAGE|DGGA G2ED EGGA G2 :|
-|:ga|bagb agef gfge dBGB|eaab a2ga eaab a2ga|
-    [1 babg agef gfge dBG2|DGGA G2ED EGGA G2:|
-	[2 babg agaf gfge dBGB|dgga g2ed egga g2gf |
+|:ga|bagb agef gfge dBGB|eaab a2ga baab a2ga|
+     babg agaf gfge dBGB|dgga g2ed egga [1 g2:| [2 g2gf ||
 |:efed Bdde gfge dBGB|EAAB A2GA EAAB A2Bd|
-     efed Bdde gfge dBAE|DGGA G2ED EGGA [1G2Bd :| [2G2!D.C.!|]
+     efed Bdde gfge dBGE|DGGA G2ED EGGA [1G2Bd :| [2G2!D.C.!|]
 `,
+			`X: 1
+T: Peggie's Wedding
+R: reel
+N: A version with chords - just a few leads; nothing definitive, as usual.
+M: 4/2
+L: 1/8
+K: Gmaj
+|:BA|"G"GFGA "G (D)"BABd "G"efed BAGA|"Am"BAAB A2GA "Am (A sus4)"EAAc BAGD|
+     "G"GFGA "G (D)"BABd "G"efed BAGE|"G"DGGA "G (C)"G2ED "G (C) (D)"EGGA "G"G2 :|
+|:ga|"G"bagb "G (D)"agef "G"gfge dBGB|"Am"eaab "Am (A sus4)"a2ga baab a2ga|
+     "G"babg "G (D)"agaf "G"gfge dBGB|"G"dgga "G (C)"g2ed "G (C) (D)"egga [1 "G"g2:| [2 "G"g2gf ||
+|:"G (C)"efed "G (D)"Bdde "G"gfge dBGB|"Am"EAAB A2GA EAAB A2Bd|
+  "G (C)"efed "G (D)"Bdde "G"gfge dBGE|"G"DDGGA "G (C)"G2ED "G (C) (D)"EGGA [1"G"G2Bd :| [2"G"G2!D.C.!|]
+`
+		],
 		parts: "AABBCC",
 		ttId: 590
 	}
