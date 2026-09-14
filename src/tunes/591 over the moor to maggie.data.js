e894a9c363950f579e43fa65d1f59569f3c9bf27
Malcolm S <malcolm_schonfield@yahoo.fr>
2026-09-14 13:21:47 +0200

    tunes

--- src/tunes/591 over the moor to maggie.data.js
+++ src/tunes/591 over the moor to maggie.data.js
@@ -1,4 +1,5 @@
 export default {
+	groups: "alora",
 	parts: "AABBCC",
 	abc: `X: 1
 T: Over The Moor To Maggie
@@ -8,7 +9,7 @@ N:from https://thesession.org/tunes/115#setting115
 N:Setting entered in thesession by user "Jeremy" in 2001 (?)
 N:*abc-tools: convert to M:4/4 & L:1/16*
 M: 4/4
-L: 1/8
+L: 1/16
 K: Gmaj
 |:DE|G2GA B2Bd efge dBAG|EAAG ABAG EAAG AcBA|
      GFGA BABd efge dBAG|DGGF GBAG EDEF G2:|
--- src/tunes/597 Hommage à Jos Bouchard.data.js
+++ src/tunes/597 Hommage à Jos Bouchard.data.js
@@ -3,6 +3,7 @@ export default {
 	groups: "ALORA",
 	abc: `X: 1
 T: Hommage À Jos Bouchard
+C:Philippe Bruneau
 O:Québec
 R: jig
 M: 12/8
@@ -16,7 +17,7 @@ B2c|:"Bm"d3 dcd BFF FBc|d3 "G"dcB "F#7"cFF F3|
 |:"D"F3 FGB A3 ABc|dcd "G"edB "D/F#"A3 "Fdim7"FAd|
 "Em"c3 Ace "A7"g3 Ace|"Em"c'ba "A7"gfe "D"fed "A7"cBA|
 "D"F3 FGB A3 ABc|dcd "G"edB "D/F#"A3 "Fdim7"FAd|
-"Em"c3 Ace "A7"g3 Ace|"Em"c'ba "A7"gfe"D"dAF D3:|`,
+"Em"c3 Ace "A7"g3 Ace|"Em"c'ba "A7"gfe "D"dAF D3:|`,
 	theSessionId: 25035,
 	theSessionSettingId: 52529,
 	parts: "AABB"
